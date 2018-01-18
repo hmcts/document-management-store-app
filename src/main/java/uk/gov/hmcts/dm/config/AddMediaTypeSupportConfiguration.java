787efedae8473f7088a26756c76c3e75a2625778
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2018-01-18 16:51:30 +0000

    codacy stuff

--- src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
@@ -20,25 +20,27 @@ public class AddMediaTypeSupportConfiguration implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String s) {
         if (bean instanceof RequestMappingHandlerAdapter) {
-            ((RequestMappingHandlerAdapter) bean).getMessageConverters().stream().filter(converter -> converter instanceof MappingJackson2HttpMessageConverter).forEach(converter -> {
-                MappingJackson2HttpMessageConverter halConverterCandidate = (MappingJackson2HttpMessageConverter) converter;
-                ObjectMapper objectMapper = halConverterCandidate.getObjectMapper();
-                if (Jackson2HalModule.isAlreadyRegisteredIn(objectMapper)) {
-                    List<MediaType> vendorSpecificTypes = new ArrayList<>(halConverterCandidate.getSupportedMediaTypes());
-                    vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_COLLECTION_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_HAL_FOLDER_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_MEDIA_TYPE);
+            ((RequestMappingHandlerAdapter) bean).getMessageConverters().stream()
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+                .forEach(converter -> {
+                    MappingJackson2HttpMessageConverter halConverterCandidate = (MappingJackson2HttpMessageConverter) converter;
+                    ObjectMapper objectMapper = halConverterCandidate.getObjectMapper();
+                    if (Jackson2HalModule.isAlreadyRegisteredIn(objectMapper)) {
+                        List<MediaType> vendorSpecificTypes = new ArrayList<>(halConverterCandidate.getSupportedMediaTypes());
+                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_COLLECTION_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_HAL_FOLDER_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_MEDIA_TYPE);
 
-                    vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_AND_METADATA_COLLECTION_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_AND_METADATA_COLLECTION_MEDIA_TYPE);
 
-                    vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_COLLECTION_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_FOLDER_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
-                    vendorSpecificTypes.add(V1MediaType.V1_AUDIT_ENTRY_MEDIA_TYPE);
-                    ((MappingJackson2HttpMessageConverter) converter).setSupportedMediaTypes(vendorSpecificTypes);
+                        vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_COLLECTION_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_FOLDER_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
+                        vendorSpecificTypes.add(V1MediaType.V1_AUDIT_ENTRY_MEDIA_TYPE);
+                        ((MappingJackson2HttpMessageConverter) converter).setSupportedMediaTypes(vendorSpecificTypes);
                 }
             });
 
