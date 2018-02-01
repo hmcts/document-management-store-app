1547c7a1d3deb56619929f805c57b03a11efdeb8
Louis Houghton <louis@louishoughton.com>
2018-02-01 14:05:59 +0000

    Fix missing bearer token log error

--- src/main/java/uk/gov/hmcts/dm/config/security/SpringSecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/security/SpringSecurityConfiguration.java
@@ -1,11 +1,16 @@
 package uk.gov.hmcts.dm.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
+import uk.gov.hmcts.reform.auth.checker.core.service.Service;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -20,10 +25,14 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private AuthCheckerServiceOnlyFilter filter;
+    private RequestAuthorizer<Service> serviceRequestAuthorizer;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        AuthCheckerServiceOnlyFilter filter = authCheckerServiceFilter();
         filter.setAuthenticationManager(authenticationManager());
 
         http.headers().cacheControl().disable();
@@ -35,14 +44,25 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .formLogin().disable()
             .logout().disable()
             .authorizeRequests()
-            .antMatchers("/swagger-ui.html",
-                    "/webjars/springfox-swagger-ui/**",
-                    "/swagger-resources/**",
-                    "/v2/**",
-                    "/health",
-                    "/info"
-            ).permitAll()
             .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+            .antMatchers("/swagger-ui.html",
+                "/webjars/springfox-swagger-ui/**",
+                "/swagger-resources/**",
+                "/v2/**",
+                "/favicon.ico",
+                "/health",
+                "/info");
+    }
+
+    private AuthCheckerServiceOnlyFilter authCheckerServiceFilter() {
+        AuthCheckerServiceOnlyFilter filter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
     }
 
 
