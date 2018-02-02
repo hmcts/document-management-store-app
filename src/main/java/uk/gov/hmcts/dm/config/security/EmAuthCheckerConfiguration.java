f8fd3ca0e181dfe5fd7072d07a52229d03e8a7f4
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2018-02-02 11:44:17 +0000

    external config for s2s names

--- src/main/java/uk/gov/hmcts/dm/config/security/EmAuthCheckerConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/security/EmAuthCheckerConfiguration.java
@@ -1,17 +1,13 @@
 package uk.gov.hmcts.dm.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
-import uk.gov.hmcts.reform.auth.checker.core.service.Service;
-import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Created by pawel on 26/06/2017.
@@ -19,8 +15,11 @@ import java.util.stream.Stream;
 @Configuration
 public class EmAuthCheckerConfiguration {
 
+    @Value("#{'${authorization.s2s-names-whitelist}'.split(',')}")
+    private List<String> s2sNamesWhiteList;
+
     @Bean
     public Function<HttpServletRequest, Collection<String>> authorizedServicesExtractor() {
-        return any -> Stream.of("sscs", "divorce", "ccd", "em_gw").collect(Collectors.toList());
+        return any -> s2sNamesWhiteList;
     }
 }
