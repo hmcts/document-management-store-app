3924b0a397530ea08bac85ee02bd81f461f3c03f
Louis Houghton <louis@louishoughton.com>
2018-02-02 09:33:34 +0000

    Fix missing bearer token log error (#25)
    Auth checker filter was being hit on every request so 
    I've excluded the URLs that we don't authenticate on
    from the Spring Security filter chain completely.

--- src/main/java/uk/gov/hmcts/dm/config/security/EmAuthCheckerConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/security/EmAuthCheckerConfiguration.java
@@ -19,42 +19,8 @@ import java.util.stream.Stream;
 @Configuration
 public class EmAuthCheckerConfiguration {
 
-//    @Bean
-//    public Function<HttpServletRequest, Optional<String>> userIdExtractor() {
-//        Pattern pattern = Pattern.compile("^/users/([^/]+)/.+$");
-//
-//        return request -> {
-//            Matcher matcher = pattern.matcher(request.getRequestURI());
-//            boolean matched = matcher.find();
-//            return Optional.ofNullable(matched ? matcher.group(1) : null);
-//        };
-//    }
-
-//    @Bean
-//    public Function<HttpServletRequest, Collection<String>> authorizedRolesExtractor() {
-//        return any -> Collections.emptyList();
-//    }
-
     @Bean
     public Function<HttpServletRequest, Collection<String>> authorizedServicesExtractor() {
         return any -> Stream.of("sscs", "divorce", "ccd", "em_gw").collect(Collectors.toList());
     }
-
-//    @Bean
-//    public AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter(RequestAuthorizer<User> userRequestAuthorizer,
-//                                                                           RequestAuthorizer<Service> serviceRequestAuthorizer,
-//                                                                           AuthenticationManager authenticationManager) {
-//        AuthCheckerServiceAndUserFilter filter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
-//        filter.setAuthenticationManager(authenticationManager);
-//        return filter;
-//    }
-
-    @Bean
-    public AuthCheckerServiceOnlyFilter authCheckerServiceFilter(RequestAuthorizer<Service> serviceRequestAuthorizer,
-                                                                        AuthenticationManager authenticationManager) {
-        AuthCheckerServiceOnlyFilter filter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
-
 }
--- src/main/java/uk/gov/hmcts/dm/config/security/SpringSecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/security/SpringSecurityConfiguration.java
@@ -2,11 +2,17 @@ package uk.gov.hmcts.dm.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
+import uk.gov.hmcts.reform.auth.checker.core.service.Service;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
+
+import java.util.Optional;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -20,30 +26,47 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private AuthCheckerServiceOnlyFilter filter;
+    private RequestAuthorizer<Service> serviceRequestAuthorizer;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private AuthCheckerServiceOnlyFilter serviceOnlyFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        filter.setAuthenticationManager(authenticationManager());
+        serviceOnlyFilter.setAuthenticationManager(authenticationManager());
 
         http.headers().cacheControl().disable();
 
         http
-            .addFilter(filter)
+            .addFilter(serviceOnlyFilter)
             .sessionManagement().sessionCreationPolicy(STATELESS).and()
             .csrf().disable()
             .formLogin().disable()
             .logout().disable()
             .authorizeRequests()
-            .antMatchers("/swagger-ui.html",
-                    "/webjars/springfox-swagger-ui/**",
-                    "/swagger-resources/**",
-                    "/v2/**",
-                    "/health",
-                    "/info"
-            ).permitAll()
             .anyRequest().authenticated();
     }
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+            .antMatchers("/swagger-ui.html",
+                "/webjars/springfox-swagger-ui/**",
+                "/swagger-resources/**",
+                "/v2/**",
+                "/favicon.ico",
+                "/health",
+                "/info");
+    }
 
+    @Autowired
+    public void setServiceOnlyFilter(Optional<AuthCheckerServiceOnlyFilter> serviceOnlyFilter) {
+        this.serviceOnlyFilter = serviceOnlyFilter.orElseGet(() -> {
+            AuthCheckerServiceOnlyFilter filter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
+            filter.setAuthenticationManager(authenticationManager);
+            return filter;
+        });
+    }
 }
