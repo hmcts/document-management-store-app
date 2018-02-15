5e3d424c938afd11241d3813a5567234a6a981a0
Alec Doran-Twyford <alec@alectronic.co>
2018-02-15 09:17:13 +0000

    aligning docker for what needed for reform api

--- testme.sh
+++ testme.sh
@@ -2,7 +2,7 @@
 #sudo apt-get install -y docker docker-compose
 clear;
 ./gradlew clean assemble --info
-./bin/test_dependency.sh
+#./bin/test_dependency.sh
 ./bin/test_unit.sh
 ./bin/test_coverage.sh
 ./bin/test_integration.sh
