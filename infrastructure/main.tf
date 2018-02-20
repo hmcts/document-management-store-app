db5b42073bc25dca6911f33e2d3be8a3c7418773
Tim Jacomb <t.jacomb@kainos.com>
2018-02-20 11:41:59 +0000

    Add dcd_evidence group to key-vault (#37)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,6 +80,7 @@ module "key-vault" {
     tenant_id           = "${var.tenant_id}"
     object_id           = "${var.jenkins_AAD_objectId}"
     resource_group_name = "${module.app.resource_group_name}"
+    product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
