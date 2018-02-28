723cdf01f4880d053e4ecf52e419b5e6703ef80d
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2018-02-28 11:50:51 +0000

    gradle: gradle test - for application tests gradle functional gradle smoke

---
+++ functionalTests/src/functionalTest/groovy/uk/gov/hmcts/dm/functional/utilities/Classifications.groovy
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.dm.functional.utilities
+
+/**
+ * Created by pawel on 13/10/2017.
+ */
+enum Classifications {
+
+    PUBLIC,PRIVATE,RESTRICTED
+}
--- functionalTests/src/test/groovy/uk/gov/hmcts/dm/it/utilities/Classifications.groovy
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.dm.it.utilities
-
-/**
- * Created by pawel on 13/10/2017.
- */
-enum Classifications {
-
-    PUBLIC,PRIVATE,RESTRICTED
-}
