0c95560cae2f37fb93f8f8ead7884bc1ba2fb772
Alec Doran-Twyford <alec@alectronic.co>
2018-03-01 16:21:54 +0000

    clean up stuff

--- testme.sh
+++ testme.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #sudo apt-get install -y docker docker-compose
 clear;
-gradlew clean assemble --info
+./gradlew clean assemble --info
 #./bin/test/test_dependency.sh
 ./bin/test/test_unit.sh
 ./bin/test/test_coverage.sh
