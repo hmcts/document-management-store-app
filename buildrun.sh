a0d9bdfc66447402fddd536734836c2244fb931d
Alec Doran-Twyford <alec@alectronic.co>
2018-03-02 16:57:37 +0000

    clean up

--- buildrun.sh
+++ buildrun.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-#sudo apt-get install -y docker docker-compose
-#sudo npm install -g nodemon
+
 clear;
 ./bin/fakeversion.sh
-#yarn install
-#yarn setup # when run in live env
-#yarn start # when run in live env
-#yarn start-dev
 ./gradlew bootRun
