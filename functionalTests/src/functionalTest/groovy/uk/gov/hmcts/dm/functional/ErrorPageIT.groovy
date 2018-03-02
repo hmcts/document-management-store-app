0bc0405c60152ee65e85a53ef91b9601e47e693c
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2018-03-02 15:50:22 +0000

    F tests - cleanup

--- functionalTests/src/functionalTest/groovy/uk/gov/hmcts/dm/functional/ErrorPageIT.groovy
+++ functionalTests/src/functionalTest/groovy/uk/gov/hmcts/dm/functional/ErrorPageIT.groovy
@@ -23,80 +23,78 @@ class ErrorPageIT extends BaseIT {
         def documentUrl = createDocumentAndGetUrlAs CITIZEN
 
         givenUnauthenticatedRequest()
-                .expect()
-                    .contentType(ContentType.JSON)
-                    .statusCode(403)
-                .when()
-                    .get(documentUrl)
+            .expect()
+                .contentType(ContentType.JSON)
+                .statusCode(403)
+            .when()
+                .get(documentUrl)
 
     }
 
-//    @Test
-//    void "EP2 As an authenticated web user trying to access an unknown document, receive HTML error page with 404"() {
-//
-//        givenRequest(CITIZEN)
-//                .accept(ContentType.HTML)
-//                .expect()
-//                    .body("html.head.title", equalTo("404 Error"))
-//                    .statusCode(404)
-//                .when()
-//                    .get('documents/XXX')
-//
-//    }
+    @Test
+    void "EP2 As an authenticated user trying to access an unknown document, receive HTML error page with 404"() {
 
-//    @Test
-//    void "EP3 As an authenticated web user trying to access document/, receive HTML error page with 405"() {
-//
-//        givenRequest(CITIZEN)
-//                .accept(ContentType.HTML)
-//                .expect()
-//                .body("html.head.title", equalTo("405 Error"))
-//                .statusCode(405)
-//                .when()
-//                .get('documents/')
-//    }
+        givenRequest(CITIZEN)
+            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .expect()
+                .contentType(ContentType.JSON)
+                .statusCode(404)
+            .when()
+                .get('documents/XXX')
 
-//    @Test
-//    void "EP4 As an authenticated web user trying to post no document, receive HTML error page with 415"() {
-//
-//        givenRequest(CITIZEN)
-//                .accept(ContentType.HTML)
-//                .expect()
-//                .body("html.head.title", equalTo("415 Error"))
-//                .statusCode(415)
-//                .when()
-//                .post('documents/')
-//    }
+    }
 
-//    @Test
-//    void "EP5 As an authenticated web user trying to post bad attachment, receive HTML error page with 415"() {
-//
-//        givenRequest(CITIZEN)
-//                .accept(ContentType.HTML)
-//                .multiPart("file", file(BAD_ATTACHMENT_1), MediaType.ALL_VALUE)
-//                .expect()
-//                .body("html.head.title", equalTo("422 Error"))
-//                .statusCode(422)
-//                .when()
-//                .post('documents/')
-//    }
+    @Test
+    void "EP3 As an authenticated user trying to access document/, receive HTML error page with 405"() {
 
-//    @Test
-//    void "EP6 As an authenticated web user but not the owner of the file, post the newer version of the file, receive HTML error page with 403"() {
-//
-//        def url = createDocumentAndGetUrlAs CITIZEN
-//
-//        createUser CITIZEN_2
-//
-//        givenRequest(CITIZEN_2)
-//                .accept(ContentType.HTML)
-//                .multiPart("file", file(ATTACHMENT_9_JPG), MediaType.IMAGE_JPEG_VALUE)
-//                .expect()
-//                .body("html.head.title", equalTo("403 Error"))
-//                .statusCode(403)
-//                .when()
-//                .post(url)
-//    }
+        givenRequest(CITIZEN)
+            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .expect()
+                .contentType(ContentType.JSON)
+                .statusCode(405)
+            .when()
+                .get('documents/')
+    }
+
+    @Test
+    void "EP4 As an authenticated user trying to post no document, receive HTML error page with 415"() {
+
+        givenRequest(CITIZEN)
+            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+        .expect()
+            .contentType(ContentType.JSON)
+            .statusCode(415)
+        .when()
+            .post('documents/')
+    }
+
+    @Test
+    void "EP5 As an authenticated user trying to post bad attachment, receive HTML error page with 415"() {
+
+        givenRequest(CITIZEN)
+            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .multiPart("file", file(BAD_ATTACHMENT_1), MediaType.ALL_VALUE)
+        .expect()
+            .contentType(ContentType.JSON)
+            .statusCode(422)
+        .when()
+            .post('documents/')
+    }
+
+    @Test
+    void "EP6 As an authenticated user but not the owner of the file, post the newer version of the file, receive JSON error page with 403"() {
+
+        def url = createDocumentAndGetUrlAs CITIZEN
+
+        givenRequest(CITIZEN_2)
+            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .multiPart("file", file(ATTACHMENT_9_JPG), MediaType.IMAGE_JPEG_VALUE)
+        .expect()
+            .contentType(ContentType.JSON)
+            .statusCode(403)
+        .when()
+            .post(url)
+    }
 
     @Test
     void "EP7 As an unauthenticated api user trying to access a document with accept JSON, receive JSON error"() {
@@ -141,20 +139,20 @@ class ErrorPageIT extends BaseIT {
             .get(documentUrl)
     }
 
-//    @Test
-//    void "EP10 As an authenticated web user but not the owner of the file, post the newer version of the file, receive HTML error page with 403"() {
-//
-//        def url = createDocumentAndGetUrlAs CITIZEN
-//
-//        givenRequest(CITIZEN)
-//            .accept(ContentType.JSON)
-//            .multiPart("file", file(ATTACHMENT_18), MediaType.APPLICATION_XML_VALUE)
-//            .expect()
-//            .body(not(containsString("<!DOCTYPE html>")))
-//            .statusCode(422)
-//            .when()
-//            .post(url)
-//    }
+    @Test
+    void "EP10 As an authenticated user but not the owner of the file, post the newer version of the file, receive JSON error page with 403"() {
+
+        def url = createDocumentAndGetUrlAs CITIZEN
+
+        givenRequest(CITIZEN)
+            .accept(ContentType.JSON)
+            .multiPart("file", file(ATTACHMENT_18), MediaType.APPLICATION_XML_VALUE)
+            .expect()
+            .contentType(ContentType.JSON)
+            .statusCode(422)
+            .when()
+            .post(url)
+    }
 
     @Test
     void "EP11 As an authenticated web user trying to post no document, receive HTML error page with 415"() {
