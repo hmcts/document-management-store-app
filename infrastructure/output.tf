eb967d14d317708da30a08d6f28707ecc31645bd
Alec Doran-Twyford <alec@alectronic.co>
2018-03-19 18:27:33 +0000

    test fixes
    Former-commit-id: 2dd73528cc88a08f6f5a23b0e8d02574d54a02c6

---
+++ infrastructure/output.tf
@@ -0,0 +1,23 @@
+output "vaultName" {
+  value = "${module.key_vault.key_vault_name}"
+}
+
+output "vaultUri" {
+  value = "${module.key_vault.key_vault_uri}"
+}
+
+output "idam_api_url" {
+  value = "${var.idam_api_url}"
+}
+
+output "s2s_url" {
+  value = "${var.s2s_url}"
+}
+
+//output "dm_store_app_url" {
+//  value = "http://${var.dm_store_app_url}-${var.env}.service.${local.ase_name}.internal"
+//}
+//
+//output "em_anno_app_url" {
+//  value = "http://${var.em_anno_app_url}-${var.env}.service.${local.ase_name}.internal"
+//}
--- performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/util/Environments.scala
+++ performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/util/Environments.scala
@@ -3,7 +3,7 @@ package uk.gov.hmcts.dm.pt.util
 object Environments {
 
   val dmStoreApp : String = scala.util.Properties.envOrElse("TEST_URL","http://localhost:4603")
-  val idamS2S : String = scala.util.Properties.envOrElse("IDAM_S2S_BASE_URI","http://localhost:4502") + "/testing-support/lease"
+  val idamS2S : String = scala.util.Properties.envOrElse("S2S_URL","http://localhost:4502") + "/testing-support/lease"
 
 
   val users: String = scala.util.Properties.envOrElse("NUMBER_OF_USERS", "500")
