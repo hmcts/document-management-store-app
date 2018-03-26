062094fa47790c53ca28ae4a4fe958649917444e
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2018-03-26 16:19:09 +0100

    missing tests

--- application/src/test/java/uk/gov/hmcts/dm/service/AzureBlobStorageClientTests.java
+++ application/src/test/java/uk/gov/hmcts/dm/service/AzureBlobStorageClientTests.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.dm.service;
 
+import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import com.microsoft.azure.storage.file.CloudFile;
@@ -11,9 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import uk.gov.hmcts.dm.exception.FileStorageException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
@@ -40,6 +45,37 @@ public class AzureBlobStorageClientTests {
         verify(cloudBlockBlob, times(1)).upload(any(InputStream.class), anyLong());
     }
 
+    @Test(expected = FileStorageException.class)
+    public void testFileUploadURISyntaxException() throws Exception {
+
+        when(cloudBlobContainer.getBlockBlobReference(any(String.class))).thenThrow(URISyntaxException.class);
+
+        azureBlobStorageClient.uploadFile(UUID.randomUUID(),
+            new MockMultipartFile("x", "x".getBytes("UTF-8")));
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testFileUploadStorageException() throws Exception {
+
+        when(cloudBlobContainer.getBlockBlobReference(any(String.class))).thenThrow(StorageException.class);
+
+        azureBlobStorageClient.uploadFile(UUID.randomUUID(),
+            new MockMultipartFile("x", "x".getBytes("UTF-8")));
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testFileUploadIOException() throws Exception {
+
+        MultipartFile file = mock(MultipartFile.class);
+
+        when(file.getInputStream()).thenThrow(IOException.class);
+
+        azureBlobStorageClient.uploadFile(UUID.randomUUID(), file);
+
+    }
+
     @Test
     public void testFileDelete() throws Exception {
 
@@ -52,6 +88,23 @@ public class AzureBlobStorageClientTests {
         verify(cloudBlockBlob, times(1)).delete();
     }
 
+    @Test(expected = FileStorageException.class)
+    public void testFileDeleteURISyntaxException() throws Exception {
+
+        when(cloudBlobContainer.getBlockBlobReference(any(String.class))).thenThrow(URISyntaxException.class);
+
+        azureBlobStorageClient.deleteFile(UUID.randomUUID());
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testFileDeleteStorageException() throws Exception {
+
+        when(cloudBlobContainer.getBlockBlobReference(any(String.class))).thenThrow(StorageException.class);
+
+        azureBlobStorageClient.deleteFile(UUID.randomUUID());
+
+    }
 
     @Test
     public void testStreamOfFile() throws Exception {
@@ -63,5 +116,37 @@ public class AzureBlobStorageClientTests {
         azureBlobStorageClient.streamFileContent(UUID.randomUUID(), mock(OutputStream.class));
 
         verify(cloudBlockBlob, times(1)).download(any(OutputStream.class));
+
     }
+
+    @Test(expected = FileStorageException.class)
+    public void testStreamOfFileURISyntaxException() throws Exception {
+
+        when(cloudBlobContainer.getBlockBlobReference(any(String.class))).thenThrow(URISyntaxException.class);
+
+        azureBlobStorageClient.streamFileContent(UUID.randomUUID(), mock(OutputStream.class));
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testStreamOfFileStorageException() throws Exception {
+
+        when(cloudBlobContainer.getBlockBlobReference(any(String.class))).thenThrow(StorageException.class);
+
+        azureBlobStorageClient.streamFileContent(UUID.randomUUID(), mock(OutputStream.class));
+
+    }
+
+    @Test(expected = StorageException.class)
+    public void testInitStorageException() throws Exception {
+        when(cloudBlobContainer.createIfNotExists()).thenThrow(StorageException.class);
+        azureBlobStorageClient.init();
+    }
+
+    @Test
+    public void testInit() throws Exception {
+        when(cloudBlobContainer.createIfNotExists()).thenReturn(true);
+        azureBlobStorageClient.init();
+    }
+
 }
--- application/src/test/java/uk/gov/hmcts/dm/service/AzureFileStorageClientTests.java
+++ application/src/test/java/uk/gov/hmcts/dm/service/AzureFileStorageClientTests.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.dm.service;
 
+import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.file.CloudFile;
 import com.microsoft.azure.storage.file.CloudFileDirectory;
 import com.microsoft.azure.storage.file.CloudFileShare;
@@ -10,9 +11,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import uk.gov.hmcts.dm.exception.FileStorageException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
@@ -43,6 +48,45 @@ public class AzureFileStorageClientTests {
         verify(cloudFile, times(1)).upload(any(InputStream.class), anyLong());
     }
 
+    @Test(expected = FileStorageException.class)
+    public void testFileUploadURISyntaxException() throws Exception {
+
+        when(cloudFileShare.getRootDirectoryReference()).thenThrow(URISyntaxException.class);
+
+        azureFileStorageClient.uploadFile(UUID.randomUUID(),
+            new MockMultipartFile("x", "x".getBytes("UTF-8")));
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testFileUploadStorageException() throws Exception {
+
+        when(cloudFileShare.getRootDirectoryReference()).thenThrow(StorageException.class);
+
+        azureFileStorageClient.uploadFile(UUID.randomUUID(),
+            new MockMultipartFile("x", "x".getBytes("UTF-8")));
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testFileUploadIOException() throws Exception {
+
+        CloudFileDirectory rootDirectory = mock(CloudFileDirectory.class);
+
+        when(cloudFileShare.getRootDirectoryReference()).thenReturn(rootDirectory);
+
+        CloudFile cloudFile = mock(CloudFile.class);
+
+        when(rootDirectory.getFileReference(any(String.class))).thenReturn(cloudFile);
+
+        MultipartFile file = mock(MultipartFile.class);
+
+        when(file.getInputStream()).thenThrow(IOException.class);
+
+        azureFileStorageClient.uploadFile(UUID.randomUUID(), file);
+
+    }
+
     @Test
     public void testFileDelete() throws Exception {
 
@@ -59,6 +103,23 @@ public class AzureFileStorageClientTests {
         verify(cloudFile, times(1)).delete();
     }
 
+    @Test(expected = FileStorageException.class)
+    public void testFileDeleteURISyntaxException() throws Exception {
+
+        when(cloudFileShare.getRootDirectoryReference()).thenThrow(URISyntaxException.class);
+
+        azureFileStorageClient.deleteFile(UUID.randomUUID());
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testFileDeleteStorageException() throws Exception {
+
+        when(cloudFileShare.getRootDirectoryReference()).thenThrow(StorageException.class);
+
+        azureFileStorageClient.deleteFile(UUID.randomUUID());
+
+    }
 
     @Test
     public void testStreamOfFile() throws Exception {
@@ -74,5 +135,35 @@ public class AzureFileStorageClientTests {
         azureFileStorageClient.streamFileContent(UUID.randomUUID(), mock(OutputStream.class));
 
         verify(cloudFile, times(1)).download(any(OutputStream.class));
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testStreamOfFileURISyntaxException() throws Exception {
+
+        when(cloudFileShare.getRootDirectoryReference()).thenThrow(URISyntaxException.class);
+
+        azureFileStorageClient.streamFileContent(UUID.randomUUID(), mock(OutputStream.class));
+
+    }
+
+    @Test(expected = FileStorageException.class)
+    public void testStreamOfFileStorageException() throws Exception {
+
+        when(cloudFileShare.getRootDirectoryReference()).thenThrow(StorageException.class);
+
+        azureFileStorageClient.streamFileContent(UUID.randomUUID(), mock(OutputStream.class));
+
+    }
+
+    @Test(expected = StorageException.class)
+    public void testInitStorageException() throws Exception {
+        when(cloudFileShare.createIfNotExists()).thenThrow(StorageException.class);
+        azureFileStorageClient.init();
+    }
+
+    @Test
+    public void testInit() throws Exception {
+        when(cloudFileShare.createIfNotExists()).thenReturn(true);
+        azureFileStorageClient.init();
     }
 }
