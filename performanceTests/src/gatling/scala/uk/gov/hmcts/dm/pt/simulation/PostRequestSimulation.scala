c28efd230f03a11b5ef7e21c55c7395df0faba82
Muhammad Husain <muhammad.husain@hmcts.net>
2018-04-10 10:44:27 +0100

    Changes made to pt

--- performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/simulation/PostRequestSimulation.scala
+++ performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/simulation/PostRequestSimulation.scala
@@ -22,10 +22,10 @@ class PostRequestSimulation extends Simulation {
 
   val testScenarios = List(postAndGetInSeq.inject(rampUsers(5) over (1 minutes)))
 
-  val randomTestScenarios = List(randomPostRequest.inject(splitUsers(150) into (rampUsers(15) over (13 seconds)) separatedBy atOnceUsers(7)), //atOnceUser(40)//rampUsers(4) over (2 minutes)),splitUsers(40) into (rampUsers(5) over (10 seconds)) separatedBy (10 seconds)
-        randomGetRequest.inject( nothingFor(2 seconds), splitUsers(150) into (rampUsers(15) over (13 seconds)) separatedBy atOnceUsers(7)))//nothingFor(5 seconds), atOnceUsers(40)))//rampUsers(4) over (2 minutes))   )
+    val randomTestScenarios = List(randomPostRequest.inject(splitUsers(150) into (rampUsers(15) over (12 seconds)) separatedBy atOnceUsers(7)), //atOnceUser(40)//rampUsers(4) over (2 minutes)),splitUsers(40) into (rampUsers(5) over (10 seconds)) separatedBy (10 seconds)
+        randomGetRequest.inject( nothingFor(2 seconds), splitUsers(150) into (rampUsers(15) over (12 seconds)) separatedBy atOnceUsers(7)))//nothingFor(5 seconds), atOnceUsers(40)))//rampUsers(4) over (2 minutes))   )
 
-//    val randomTestScenarios = List(randomPostRequest.inject(splitUsers(300) into (rampUsers(32) over (10 seconds)) separatedBy atOnceUsers(7)), //atOnceUser(40)//rampUsers(4) over (2 minutes)),splitUsers(40) into (rampUsers(5) over (10 seconds)) separatedBy (10 seconds)
+    //    val randomTestScenarios = List(randomPostRequest.inject(splitUsers(300) into (rampUsers(32) over (10 seconds)) separatedBy atOnceUsers(7)), //atOnceUser(40)//rampUsers(4) over (2 minutes)),splitUsers(40) into (rampUsers(5) over (10 seconds)) separatedBy (10 seconds)
 //        randomGetRequest.inject( nothingFor(2 seconds), splitUsers(300) into (rampUsers(32) over (9 seconds)) separatedBy atOnceUsers(7)))//nothingFor(5 seconds), atOnceUsers(40)))//rampUsers(4) over (2 minutes))   )
     //  val testScenarioForPostRecords = List(PostRequest.postRequestScenario.inject(atOnceUsers(1),rampUsers(38) over (2 minute)))
 
