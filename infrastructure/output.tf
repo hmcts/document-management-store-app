0fea136855d206b2f82154995c324fd68f75411c
Alec Doran-Twyford <alec@alectronic.co>
2018-04-13 21:30:08 +0100

    Feature/smoke test env va pass (#74)
    Former-commit-id: e9b10215d62ff82bdb65b8bcc2ce622477e0f8cd

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -25,3 +25,36 @@ output "dm_store_app_url" {
 output "em_anno_app_url" {
   value = "http://${var.em_anno_app_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
 }
+
+output "enable_idam_health_check" {
+  value = "${var.enable_idam_healthcheck}"
+}
+
+output "enable_metadata_search" {
+  value = "${var.enable_metadata_search}"
+}
+
+output "enable_idam_healthcheck" {
+  value = "${var.enable_idam_healthcheck}"
+}
+
+output "enable_document_and_metadata_upload" {
+  value = "${var.enable_document_and_metadata_upload}"
+}
+
+output "enable_folder_api" {
+  value = "${var.enable_folder_api}"
+}
+
+output "enable_delete" {
+  value = "${var.enable_delete}"
+}
+
+output "enable_ttl" {
+  value = "${var.enable_ttl}"
+}
+
+output "enable_thumbnail" {
+  value = "${var.enable_thumbnail}"
+}
+
