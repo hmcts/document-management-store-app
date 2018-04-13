33e1ceadb1378355df2b038563726711b4662699
Alec Doran-Twyford <alec@alectronic.co>
2018-04-13 12:32:33 +0100

    print

--- performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/util/idamTokenGenerator.scala
+++ performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/util/idamTokenGenerator.scala
@@ -5,20 +5,23 @@ import io.restassured.RestAssured
 
 object idamTokenGenerator {
 
-    val params = Map(
-        ("microservice", "em_gw"),
-        ("oneTimePassword", new GoogleAuthenticator().getTotpPassword(Environments.s2sSecret))
-    )
-
     def generateS2SToken(): String = {
-        RestAssured.given
+        val request = RestAssured.given
             .relaxedHTTPSValidation
             .baseUri(Environments.idamS2S)
             //          .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .header("Content-Type", "application/json")
-            .body(params)
+            .body(
+                Map(
+                    ("microservice", "em_gw"),
+                    ("oneTimePassword", new GoogleAuthenticator().getTotpPassword(Environments.s2sSecret))
+                )
+            )
             .post("/lease")
-          .andReturn().toString
+            .andReturn().toString
+
+        print("generateS2SToken===============" + request)
+        request
     }
 }
 
