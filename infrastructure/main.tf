75f999e0511e59ab658e18bce63216687bf772a7
Louis Houghton <louis@louishoughton.com>
2018-04-18 11:58:04 +0100

    Set is_frontend to true to use custom domain (#82)
    Former-commit-id: 9dafb7ead4be96580062e60840977127899e4361

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,7 +15,7 @@ module "app" {
   ilbIp = "${var.ilbIp}"
   subscription = "${var.subscription}"
   capacity     = "${var.capacity}"
-  is_frontend = false
+  is_frontend = true #It's not front end but we need it so we can have a custom URL at the moment.
   additional_host_name = "${local.app_full_name}-${var.env}.service.${var.env}.platform.hmcts.net"
   https_only="false"
 
