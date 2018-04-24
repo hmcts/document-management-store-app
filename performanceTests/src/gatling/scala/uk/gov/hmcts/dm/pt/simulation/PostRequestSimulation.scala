08d6357d637da368c97839ed519bb6c298d318aa
Muhammad Husain <muhammad.husain@hmcts.net>
2018-04-24 10:55:35 +0100

    Changed the file size based on prod and trying slow rampup scenario.

--- performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/simulation/PostRequestSimulation.scala
+++ performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/simulation/PostRequestSimulation.scala
@@ -26,8 +26,8 @@ class PostRequestSimulation extends Simulation {
         randomGetRequest.inject( nothingFor(20 seconds), splitUsers(400) into (rampUsers(50) over (30 seconds)) separatedBy atOnceUsers(7)))//nothingFor(5 seconds), atOnceUsers(40)))//rampUsers(4) over (2 minutes))   )
 
 
-    val loadScenarios = List(randomPostRequest.inject(rampUsers(10) over (30 seconds)), //atOnceUser(40)//rampUsers(4) over (2 minutes)),splitUsers(40) into (rampUsers(5) over (10 seconds)) separatedBy (10 seconds)
-        randomGetRequest.inject( nothingFor(10 seconds), rampUsers(10) over (30 seconds)))
+    val prodLikeScenarios = List(randomPostRequest.inject(splitUsers(100) into (rampUsers(1) over (4 seconds)) separatedBy atOnceUsers(1)), //atOnceUser(40)//rampUsers(4) over (2 minutes)),splitUsers(40) into (rampUsers(5) over (10 seconds)) separatedBy (10 seconds)
+        randomGetRequest.inject( nothingFor(20 seconds), splitUsers(100) into (rampUsers(1) over (4 seconds)) separatedBy atOnceUsers(1)))
 //        randomPostRequest.inject(rampUsers(20) over (30 seconds)), //atOnceUser(40)//rampUsers(4) over (2 minutes)),splitUsers(40) into (rampUsers(5) over (10 seconds)) separatedBy (10 seconds)
 //        randomGetRequest.inject( nothingFor(10 seconds), rampUsers(20) over (30 seconds)))
 
@@ -39,7 +39,7 @@ class PostRequestSimulation extends Simulation {
 
 //  setUp(testScenarios)
 //    setUp(randomTestScenarios)
-    setUp(loadScenarios)
+    setUp(prodLikeScenarios)
     .protocols(httpConf)
     .maxDuration(5 minutes)
     .assertions(
