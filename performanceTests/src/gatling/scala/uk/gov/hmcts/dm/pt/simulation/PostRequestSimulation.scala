36cfe13971242dc6a7421674deb20b17fd850c69
Muhammad Husain <muhammad.husain@hmcts.net>
2018-04-30 12:11:11 +0100

    Tunning

--- performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/simulation/PostRequestSimulation.scala
+++ performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/simulation/PostRequestSimulation.scala
@@ -47,13 +47,4 @@ class PostRequestSimulation extends Simulation {
     .assertions(
       global.responseTime.max.lte(Environments.maxResponseTime.toInt),
       global.successfulRequests.percent.gte(99))
-
-
-  //  val httpConf = http
-  //    .baseURL("http://computer-database.gatling.io") // Here is the root for all relative URLs
-  //    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-  //    .acceptEncodingHeader("gzip, deflate")
-  //    .acceptLanguageHeader("en-US,en;q=0.5")
-  //    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
-
 }
--- performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/util/Headers.scala
+++ performanceTests/src/gatling/scala/uk/gov/hmcts/dm/pt/util/Headers.scala
@@ -4,14 +4,6 @@ object Headers {
 
   val commonHeader: Map[String, String] = Map(
     "Accept" -> "application/vnd.uk.gov.hmcts.dm.document.v1+json"
-    //    "Accept-Charset" -> "ISO-8859-1,utf-8;q=0.7,*;q=0.7"
-    //    "Authorization" -> "user2",
-    //    "ServiceAuthorization" ->"sscs"
-
-    //    Uncomment the code below for testing with IDAM
-    //    "Accept" -> "application/json"
-    //    "Authorization" -> idamTokenGenerator.generateUserToken(),
-    //    "ServiceAuthorization" -> token
   )
 
 }
