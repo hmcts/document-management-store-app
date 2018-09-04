f960a80e68e95eefa5f126c103b54292bbdc524f
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-09-04 21:08:32 +0100

    Vault: copy keys into shared vault
    Former-commit-id: 50cca1ac90204a017bbc794bb81098130afecbc7

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -142,31 +142,31 @@ data "azurerm_key_vault" "dm_shared_vault" {
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name = "${local.app_full_name}-POSTGRES-USER"
   value = "${module.db.user_name}"
-  vault_uri = "${module.key_vault.key_vault_uri}"
+  vault_uri = "${data.azurerm_key_vault.dm_shared_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name = "${local.app_full_name}-POSTGRES-PASS"
   value = "${module.db.postgresql_password}"
-  vault_uri = "${module.key_vault.key_vault_uri}"
+  vault_uri = "${data.azurerm_key_vault.dm_shared_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name = "${local.app_full_name}-POSTGRES-HOST"
   value = "${module.db.host_name}"
-  vault_uri = "${module.key_vault.key_vault_uri}"
+  vault_uri = "${data.azurerm_key_vault.dm_shared_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   name = "${local.app_full_name}-POSTGRES-PORT"
   value = "${module.db.postgresql_listen_port}"
-  vault_uri = "${module.key_vault.key_vault_uri}"
+  vault_uri = "${data.azurerm_key_vault.dm_shared_vault.vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name = "${local.app_full_name}-POSTGRES-DATABASE"
   value = "${module.db.postgresql_database}"
-  vault_uri = "${module.key_vault.key_vault_uri}"
+  vault_uri = "${data.azurerm_key_vault.dm_shared_vault.vault_uri}"
 }
 
 data "azurerm_key_vault_secret" "storageaccount_primary_connection_string" {
