5dc9b8644a8846b62ddfc208c47efeb6f0aa0213
Adrian Cho <waihung.cho@hmcts.net>
2018-09-12 11:59:06 +0100

    RDM-2773 write to os

--- application/src/test/java/uk/gov/hmcts/dm/domain/DocumentContentVersionTest.java
+++ application/src/test/java/uk/gov/hmcts/dm/domain/DocumentContentVersionTest.java
@@ -2,9 +2,6 @@ package uk.gov.hmcts.dm.domain;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,26 +11,26 @@ import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
-@RunWith(MockitoJUnitRunner.class)
 public class DocumentContentVersionTest {
 
-    @Mock
     private StoredDocument item;
 
     private static final String FILE_NAME = "The Little Prince";
     private static final String FILE_ORIGINAL_FILE_NAME = "Le Petit Prince";
     private static final String FILE_TYPE = "avec les illustrations originales de l'auteur";
     private static final byte[] FILE_CONTENT = "Antoine de Saint-Exupéry".getBytes(defaultCharset());
+    private static final String USER_ID = "user id";
 
     private static final MultipartFile FILE = new MockMultipartFile(FILE_NAME, FILE_ORIGINAL_FILE_NAME, FILE_TYPE, FILE_CONTENT);
 
     @Before
     public void setUp() {
+        item = new StoredDocument();
     }
 
     @Test
     public void deprecatedDocumentContentIsUsed() {
-        final DocumentContentVersion documentContentVersion = new DocumentContentVersion(item, FILE, "userId", false);
+        final DocumentContentVersion documentContentVersion = new DocumentContentVersion(item, FILE, USER_ID, false);
 
         assertDocumentContentVersionCommon(documentContentVersion);
         assertThat(documentContentVersion.getDocumentContent(), not(nullValue()));
@@ -41,7 +38,7 @@ public class DocumentContentVersionTest {
 
     @Test
     public void deprecatedDocumentContentIsNotUsed() {
-        final DocumentContentVersion documentContentVersion = new DocumentContentVersion(item, FILE, "userId", true);
+        final DocumentContentVersion documentContentVersion = new DocumentContentVersion(item, FILE, USER_ID, true);
 
         assertDocumentContentVersionCommon(documentContentVersion);
         assertThat(documentContentVersion.getDocumentContent(), is(nullValue()));
@@ -53,7 +50,7 @@ public class DocumentContentVersionTest {
         assertThat(documentContentVersion.getOriginalDocumentName(), is(FILE_ORIGINAL_FILE_NAME));
         assertThat(documentContentVersion.getSize(), is(Long.valueOf(FILE_CONTENT.length)));
         assertThat(documentContentVersion.getContentUri(), is(nullValue()));
-        assertThat(documentContentVersion.getCreatedBy(), is("userId"));
+        assertThat(documentContentVersion.getCreatedBy(), is(USER_ID));
     }
 
 }
