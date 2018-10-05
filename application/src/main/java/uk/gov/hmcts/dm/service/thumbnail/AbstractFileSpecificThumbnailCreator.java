8a9fb942766211de98924bb1302c34a5e0042298
Valentin Laurin <me@vlaurin.eu>
2018-10-05 15:23:30 +0100

    RDM-3121: Add thumbnail creation from Azure Blob
    Add support for Azure Blob store when creating thumbnails
    Test thumbnail generation
    
    
    Former-commit-id: b0c88c2ae6999afba53ad3c46a6f02d790b45590

--- application/src/main/java/uk/gov/hmcts/dm/service/thumbnail/AbstractFileSpecificThumbnailCreator.java
+++ application/src/main/java/uk/gov/hmcts/dm/service/thumbnail/AbstractFileSpecificThumbnailCreator.java
@@ -2,19 +2,44 @@ package uk.gov.hmcts.dm.service.thumbnail;
 
 import uk.gov.hmcts.dm.domain.DocumentContentVersion;
 import uk.gov.hmcts.dm.exception.CantCreateThumbnailException;
+import uk.gov.hmcts.dm.service.BlobStorageReadService;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.util.Locale;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 public abstract class AbstractFileSpecificThumbnailCreator implements ThumbnailCreator {
+
+    private final BlobStorageReadService blobStorageReadService;
+
+    public AbstractFileSpecificThumbnailCreator(BlobStorageReadService blobStorageReadService) {
+        this.blobStorageReadService = blobStorageReadService;
+    }
+
     @Override
     public InputStream getThumbnail(DocumentContentVersion documentContentVersion) {
         try {
-            InputStream inputStream = documentContentVersion.getDocumentContent().getData().getBinaryStream();
+            InputStream inputStream;
+            if (isBlank(documentContentVersion.getContentUri())) {
+                inputStream = documentContentVersion.getDocumentContent().getData().getBinaryStream();
+            } else {
+                // Pipe blob store output into thumbnail creator input
+                final PipedInputStream in = new PipedInputStream();
+                final PipedOutputStream out = new PipedOutputStream(in);
+
+                Thread loadThread = new Thread(() -> blobStorageReadService.loadBlob(documentContentVersion, out));
+                loadThread.start();
+
+                inputStream = in;
+            }
+
             BufferedImage bufferedImage = getImg(inputStream);
             ByteArrayOutputStream os = new ByteArrayOutputStream();
             ImageIO.write(bufferedImage, ThumbnailFormats.JPG.toString().toLowerCase(Locale.UK), os);
