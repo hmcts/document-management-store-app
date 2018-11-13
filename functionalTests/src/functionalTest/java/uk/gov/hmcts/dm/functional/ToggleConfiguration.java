ef3951615c0dc633ac9db90e4a77a0ca8485e209
Valentin Laurin <me@vlaurin.eu>
2018-11-13 23:50:27 +0000

    RDM-3551: Add toggles to functional tests config
    Add toggle config to application properties
    Add toggle config to spring configuration

--- functionalTests/src/functionalTest/java/uk/gov/hmcts/dm/functional/ToggleConfiguration.java
+++ functionalTests/src/functionalTest/java/uk/gov/hmcts/dm/functional/ToggleConfiguration.java
@@ -1,18 +1,34 @@
-package uk.gov.hmcts.dm.config;
+package uk.gov.hmcts.dm.functional;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "toggle")
-@Data
 public class ToggleConfiguration {
-
     private boolean metadatasearchendpoint;
     private boolean documentandmetadatauploadendpoint;
     private boolean folderendpoint;
     private boolean includeidamhealth;
     private boolean ttl;
 
+    public boolean isMetadatasearchendpoint() {
+        return metadatasearchendpoint;
+    }
+
+    public boolean isDocumentandmetadatauploadendpoint() {
+        return documentandmetadatauploadendpoint;
+    }
+
+    public boolean isFolderendpoint() {
+        return folderendpoint;
+    }
+
+    public boolean isIncludeidamhealth() {
+        return includeidamhealth;
+    }
+
+    public boolean isTtl() {
+        return ttl;
+    }
 }
