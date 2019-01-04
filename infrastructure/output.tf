f49410860d6b3a9df3e47bd482b17cbd828c9623
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2019-01-04 11:47:05 +0000

    Remove vaultUri from Terraform output
    Outputing forces use of that vault and thus blocks the multivault setup.
    
    RDM-2931

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -6,10 +6,6 @@ output "vaultName" {
   value = "${local.vaultName}"
 }
 
-output "vaultUri" {
-  value = "${data.azurerm_key_vault.ccd_shared_vault.vault_uri}"
-}
-
 output "idam_api_url" {
   value = "${var.idam_api_url}"
 }
