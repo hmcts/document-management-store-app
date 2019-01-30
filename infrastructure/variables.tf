13d2c187de097f9ad1b69f2bf9e655a1f22fc92b
Linus Norton <linusnorton@gmail.com>
2019-01-30 17:14:25 +0000

    Add stitching API to s2s whitelist
    Former-commit-id: 46c213e8bb3ec82945663890a667b5281db98c04

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -185,7 +185,7 @@ variable "dm_multipart_whitelist_ext" {
 }
 
 variable "s2s_names_whitelist" {
-  default = "em_api,em_gw,ccd_gw,ccd_data,sscs,sscs_bulkscan,divorce_document_upload,divorce_frontend,divorce_document_generator,probate_backend,jui_webapp,pui_webapp,cmc_claim_store,bulk_scan_processor,em_npa_app,bulk_scan_orchestrator,fpl_case_service,finrem_document_generator,iac"
+  default = "em_api,em_gw,ccd_gw,ccd_data,sscs,sscs_bulkscan,divorce_document_upload,divorce_frontend,divorce_document_generator,probate_backend,jui_webapp,pui_webapp,cmc_claim_store,bulk_scan_processor,em_npa_app,bulk_scan_orchestrator,fpl_case_service,finrem_document_generator,iac,em_stitching_api"
 }
 
 variable "case_worker_roles" {
