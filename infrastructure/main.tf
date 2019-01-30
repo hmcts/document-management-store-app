dd3a8de77b53bf7af881c21b11f12524c1a3e744
Tim Jacomb <t.jacomb@kainos.com>
2019-01-30 10:48:14 +0000

    Enable local cache (#249)
    Recommended by microsoft
    
    Former-commit-id: 8ff845e172819e2eea6a01867c9ad33a2a3c8ece

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -45,7 +45,7 @@ module "app" {
   common_tags  = "${var.common_tags}"
   asp_name = "${(var.asp_name == "use_shared") ? local.sharedAppServicePlan : var.asp_name}"
   asp_rg = "${(var.asp_rg == "use_shared") ? local.sharedASPResourceGroup : var.asp_rg}"
-  website_local_cache_sizeinmb = 0
+  website_local_cache_sizeinmb = 1600
 
   app_settings = {
     POSTGRES_HOST = "${module.db.host_name}"
