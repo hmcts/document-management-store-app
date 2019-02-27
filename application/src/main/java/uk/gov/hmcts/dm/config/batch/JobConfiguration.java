3b7433bd6aa9a4a7e0420587300a1e5375fd416e
mutlu <mutlu@mutlus-mbp>
2019-02-27 21:58:01 +0000

    remove id incrementer as it is already  unneeded in our batch run config

--- application/src/main/java/uk/gov/hmcts/dm/config/batch/JobConfiguration.java
+++ application/src/main/java/uk/gov/hmcts/dm/config/batch/JobConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -40,7 +39,7 @@ public class JobConfiguration {
     public Job job() {
         return jobBuilderFactory
             .get("documentRetentionJob")
-            .incrementer(new RunIdIncrementer())
+            .incrementer(null)
             .flow(step1())
             .end()
             .build();
