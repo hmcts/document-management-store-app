729a2ad40ddb1445313bf5935b659a9445ef7ec8
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2019-03-01 11:42:17 +0000

    Remove unused 'core-infra' remote state
    RDM-4223

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,14 +1,3 @@
 terraform {
   backend "azurerm" {}
 }
-
-data "terraform_remote_state" "core_apps_infrastructure" {
-  backend = "azurerm"
-
-  config {
-    resource_group_name  = "mgmt-state-store-${var.subscription}"
-    storage_account_name = "mgmtstatestore${var.subscription}"
-    container_name       = "mgmtstatestorecontainer${var.env}"
-    key                  = "core-infra/${var.env}/terraform.tfstate"
-  }
-}
