3de592565399436acfeb9dc3ceb233b251828430
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2019-03-01 11:40:42 +0000

    Remove 'core-compute' remote Terraform state
    RDM-4223

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -12,14 +12,3 @@ data "terraform_remote_state" "core_apps_infrastructure" {
     key                  = "core-infra/${var.env}/terraform.tfstate"
   }
 }
-
-data "terraform_remote_state" "core_apps_compute" {
-  backend = "azurerm"
-
-  config {
-    resource_group_name  = "mgmt-state-store-${var.subscription}"
-    storage_account_name = "mgmtstatestore${var.subscription}"
-    container_name       = "mgmtstatestorecontainer${var.env}"
-    key                  = "core-compute/${var.env}/terraform.tfstate"
-  }
-}
