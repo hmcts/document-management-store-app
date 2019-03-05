2504ad424dbba2cd9c5f476aeed8e23f44aa09bd
mutlu <mutlu@172.16.166.120>
2019-03-05 12:20:36 +0000

    added bean config for XHF features to take effect

--- application/src/main/java/uk/gov/hmcts/dm/config/WebConfig.java
+++ application/src/main/java/uk/gov/hmcts/dm/config/WebConfig.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.dm.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
@@ -18,5 +20,13 @@ public class WebConfig {
         FixedLocaleResolver localeResolver = new FixedLocaleResolver();
         localeResolver.setDefaultLocale(Locale.UK);
         return localeResolver;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter()
+    {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        return bean;
     }
 }
