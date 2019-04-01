d9f135e3cc69297b3d6d969b13b017d5e580574c
Javier Carretero <javi_1986@hotmail.com>
2019-04-01 12:19:22 +0100

    Added ethos repl service to the whitelist

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -191,7 +191,8 @@ variable "dm_multipart_whitelist_ext" {
 }
 
 variable "s2s_names_whitelist" {
-  default = "em_api,em_gw,ccd_gw,ccd_data,sscs,sscs_bulkscan,divorce_document_upload,divorce_frontend,divorce_document_generator,probate_backend,jui_webapp,pui_webapp,cmc_claim_store,bulk_scan_processor,em_npa_app,bulk_scan_orchestrator,fpl_case_service,finrem_document_generator,iac,em_stitching_api,dg_docassembly_api"
+  default = "em_api,em_gw,ccd_gw,ccd_data,sscs,sscs_bulkscan,divorce_document_upload,divorce_frontend,divorce_document_generator,probate_backend,jui_webapp,pui_webapp,cmc_claim_store,bulk_scan_processor,em_npa_app,bulk_scan_orchestrator,fpl_case_service,finrem_document_generator,iac,em_stitching_api,dg_docassembly_api, ethos_repl_service"
+
 }
 
 variable "case_worker_roles" {
