e82b22cf513c87ddc359bc34779084861750ba0c
Linus Norton <linusnorton@gmail.com>
2020-01-21 14:47:30 +0000

    clean imports

--- application/src/main/java/uk/gov/hmcts/dm/service/BlobStorageWriteService.java
+++ application/src/main/java/uk/gov/hmcts/dm/service/BlobStorageWriteService.java
@@ -15,14 +15,9 @@ import uk.gov.hmcts.dm.exception.FileStorageException;
 import uk.gov.hmcts.dm.repository.DocumentContentVersionRepository;
 
 import javax.validation.constraints.NotNull;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.UUID;
-
-import static org.apache.commons.io.IOUtils.toByteArray;
-import static org.springframework.security.core.token.Sha512DigestUtils.shaHex;
 
 @Slf4j
 @Service
