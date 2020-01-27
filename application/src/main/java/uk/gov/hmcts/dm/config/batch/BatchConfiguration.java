a3e8c44dac3a9adb66a225be640eee4215dfa99c
Linus Norton <linusnorton@gmail.com>
2020-01-27 13:44:31 +0000

    Revert "Revert "Revert "Revert "Shedlock (#483)" (#495)" (#496)""
    This reverts commit 2581cc2693f427df955a4029be83c7a677c473a6.

--- application/src/main/java/uk/gov/hmcts/dm/config/batch/BatchConfiguration.java
+++ application/src/main/java/uk/gov/hmcts/dm/config/batch/BatchConfiguration.java
@@ -1,49 +1,144 @@
 package uk.gov.hmcts.dm.config.batch;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.core.SchedulerLock;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.PlatformTransactionManager;
-import uk.gov.hmcts.dm.exception.AppConfigurationException;
+import uk.gov.hmcts.dm.domain.StoredDocument;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Date;
 
-@Configuration
 @EnableBatchProcessing
-@EnableConfigurationProperties(BatchProperties.class)
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT5M")
+@Configuration
+@ConditionalOnProperty("toggle.ttl")
 public class BatchConfiguration {
 
-    @Bean
-    public JobRepository jobRepository(
-         DataSource dataSource,
-         @Qualifier("transactionManager") PlatformTransactionManager transactionManager) {
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
 
-        try {
-            JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
-            factory.setDataSource(dataSource);
-            factory.setTransactionManager(transactionManager);
-            factory.afterPropertiesSet();
-            return factory.getObject();
-        } catch (Exception e) {
-            throw new AppConfigurationException("Could not create 'jobRepository' bean", e);
-        }
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    public JobLauncher jobLauncher;
+
+    @Autowired
+    public DeleteExpiredDocumentsProcessor deleteExpiredDocumentsProcessor;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    @Qualifier("transactionManager") PlatformTransactionManager transactionManager;
+
+    @Value("${spring.batch.historicExecutionsRetentionMilliseconds}")
+    int historicExecutionsRetentionMilliseconds;
+
+    @Scheduled(fixedRateString = "${spring.batch.document-task-milliseconds}")
+    @SchedulerLock(name = "${task.env}")
+    public void schedule() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        jobLauncher
+            .run(processDocument(step1()), new JobParametersBuilder()
+            .addDate("date", new Date())
+            .toJobParameters());
+
+    }
+
+    @Scheduled(fixedDelayString = "${spring.batch.historicExecutionsRetentionMilliseconds}")
+    @SchedulerLock(name = "${task.env}-historicExecutionsRetention")
+    public void scheduleCleanup() throws JobParametersInvalidException,
+        JobExecutionAlreadyRunningException,
+        JobRestartException,
+        JobInstanceAlreadyCompleteException {
+
+        jobLauncher.run(clearHistoryData(), new JobParametersBuilder()
+            .addDate("date", new Date())
+            .toJobParameters());
 
     }
 
     @Bean
-    public JobLauncher jobLauncher(JobRepository jobRepository) {
-        SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
-        simpleJobLauncher.setJobRepository(jobRepository);
-        return simpleJobLauncher;
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(dataSource);
     }
 
+    @Bean
+    public JpaPagingItemReader undeletedDocumentsWithTtl() {
+        return new JpaPagingItemReaderBuilder<StoredDocument>()
+            .name("documentTaskReader")
+            .entityManagerFactory(entityManagerFactory)
+            .queryString("select d from StoredDocument d JOIN FETCH d.documentContentVersions "
+                + "where d.hardDeleted = false AND d.ttl < current_timestamp()")
+            .pageSize(100)
+            .build();
+    }
+
+
+    @Bean
+    public JpaItemWriter itemWriter() {
+        JpaItemWriter writer = new JpaItemWriter<StoredDocument>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+        return writer;
+    }
+
+    @Bean
+    public Job processDocument(Step step1) {
+        return jobBuilderFactory.get("processDocumentJob")
+            .flow(step1)
+            .end()
+            .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+            .transactionManager(transactionManager)
+            .<StoredDocument, StoredDocument>chunk(10)
+            .reader(undeletedDocumentsWithTtl())
+            .processor(deleteExpiredDocumentsProcessor)
+            .writer(itemWriter())
+            .build();
+
+    }
+
+    @Bean
+    public Job clearHistoryData() {
+        return jobBuilderFactory.get("clearHistoricBatchExecutions")
+            .flow(stepBuilderFactory.get("deleteAllExpiredBatchExecutions")
+                .tasklet(new RemoveSpringBatchHistoryTasklet(historicExecutionsRetentionMilliseconds, jdbcTemplate))
+                .build()).build().build();
+    }
 
 
 }
