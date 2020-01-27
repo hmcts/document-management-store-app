a15129bdb859cf3b76f2d833fdd90bc8cb693768
Linus Norton <linusnorton@gmail.com>
2020-01-27 11:51:55 +0000

    Revert "remove transactional annotations so that spring boot and spring batch don't share a transaction manager (#505)"
    This reverts commit 3c408adf767a78973e2389ec523de303c70d669d [formerly 1a697afa3d64ff05de3df5f8ca88fc58448fd7d2].
    
    
    Former-commit-id: 0c62e69c7ae91ca6c24f39648726eb46da231049

--- application/src/main/java/uk/gov/hmcts/dm/DmApp.java
+++ application/src/main/java/uk/gov/hmcts/dm/DmApp.java
@@ -6,14 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableCircuitBreaker
 @EnableHystrixDashboard
-@EnableScheduling
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class DmApp {
 
