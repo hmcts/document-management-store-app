a87f1d049bb69f6ff64d6e25f75459a3b0897f19
Linus Norton <linusnorton@gmail.com>
2020-01-28 08:57:21 +0000

    re-enable all the things

--- application/src/main/java/uk/gov/hmcts/dm/config/batch/BatchConfiguration.java
+++ application/src/main/java/uk/gov/hmcts/dm/config/batch/BatchConfiguration.java
@@ -63,10 +63,10 @@ public class BatchConfiguration {
     @Scheduled(fixedRateString = "${spring.batch.document-task-milliseconds}")
     @SchedulerLock(name = "${task.env}")
     public void schedule() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
-//        jobLauncher
-//            .run(processDocument(step1()), new JobParametersBuilder()
-//            .addDate("date", new Date())
-//            .toJobParameters());
+        jobLauncher
+            .run(processDocument(step1()), new JobParametersBuilder()
+            .addDate("date", new Date())
+            .toJobParameters());
 
     }
 
@@ -77,9 +77,9 @@ public class BatchConfiguration {
         JobRestartException,
         JobInstanceAlreadyCompleteException {
 
-//        jobLauncher.run(clearHistoryData(), new JobParametersBuilder()
-//            .addDate("date", new Date())
-//            .toJobParameters());
+        jobLauncher.run(clearHistoryData(), new JobParametersBuilder()
+            .addDate("date", new Date())
+            .toJobParameters());
 
     }
 
