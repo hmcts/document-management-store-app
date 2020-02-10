5c12ef9a0fac3afaf14b44412a8e1655211b95a5
Linus Norton <linusnorton@gmail.com>
2020-02-10 15:36:08 +0000

    oh, the wraith of checkstyle

--- application/src/test/java/uk/gov/hmcts/dm/security/PermissionEvaluatorImplTests.java
+++ application/src/test/java/uk/gov/hmcts/dm/security/PermissionEvaluatorImplTests.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.dm.repository.RepositoryFinder;
 import uk.gov.hmcts.dm.security.domain.DomainPermissionEvaluator;
