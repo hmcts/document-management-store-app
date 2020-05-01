7650ab3a73f63ffd38428fd2899146ab3e4ca431
Linus Norton <linusnorton@gmail.com>
2020-05-01 13:17:24 +0100

    removed redundant shell scripts

--- bin/test/test_coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-./gradlew jacocoTestReport --info
-
-xdg-open application/build/reports/jacoco/test/html/index.html
-open application/build/reports/jacoco/test/html/index.html
-start "" application/build/reports/jacoco/test/html/index.html
-
-#./gradlew sonarqube -Dsonar.host.url=$SONARQUBE_URL
--- bin/test/test_dependency.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-./gradlew dependencyCheckAnalyze -DdependencyCheck.failBuild=false
-
-xdg-open application/build/reports/dependency-check-report.html
-open application/build/reports/dependency-check-report.html
-start "" application/build/reports/dependency-check-report.html
