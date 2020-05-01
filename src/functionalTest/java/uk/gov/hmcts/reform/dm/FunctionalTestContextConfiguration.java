574e52e356ac463b2ec4f7187445416fca8514da
Linus Norton <linusnorton@gmail.com>
2020-05-01 14:39:16 +0100

    checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/dm/FunctionalTestContextConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/dm/FunctionalTestContextConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.dm;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
