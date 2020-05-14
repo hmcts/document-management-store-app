2d65ef1933ebebcda3714ad4b38b5295ab39ac63
isha2040 <58481974+isha2040@users.noreply.github.com>
2020-05-14 10:55:44 +0100

    EM-2724 Changes for serenity Integration

--- src/functionalTest/groovy/uk/gov/hmcts/dm/functional/MetadataMigrationIT.groovy
+++ src/functionalTest/groovy/uk/gov/hmcts/dm/functional/MetadataMigrationIT.groovy
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.dm.functional
 
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.test.context.junit4.SpringRunner
 import uk.gov.hmcts.dm.functional.utilities.Classifications
 
 import static org.hamcrest.Matchers.*
@@ -11,7 +11,7 @@ import static org.junit.Assume.assumeTrue
 import static org.junit.Assert.assertThat;
 
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 class MetadataMigrationIT extends BaseIT {
 
     @Test
