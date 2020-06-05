518f5f751d68cebaa7f8689e523aa8a03d92be3b
Jason Paige <jason.r.paige@gmail.com>
2020-06-05 11:09:53 +0100

    Explanation for override and remove springfox

--- src/main/java/uk/gov/hmcts/dm/errorhandler/ApiErrorController.java
+++ src/main/java/uk/gov/hmcts/dm/errorhandler/ApiErrorController.java
@@ -29,6 +29,13 @@ public class ApiErrorController extends BasicErrorController {
             return new ResponseEntity<>(status);
         }
         Map<String, Object> body = getErrorAttributes(request, getErrorAttributeOptions(request, MediaType.ALL));
+
+        // Set the status again for the legacy error handling code to work
+        // This was changed by spring here: https://github.com/spring-projects/spring-boot/commit/a354657acef83919cb3685c7dd50f2a42156efca#r35879840
+        // Clearly the error handling in this service is not correct but it's complex enough to take a long time
+        // to replicate the behaviour doing it 'the right way'
+        // For the sake of time, I've overridden this method to achieve the same functionality as before and raised
+        // a Jira ticket as a tech debt item to resolve this in the future.
         status = getStatus(request);
         return new ResponseEntity<>(body, status);
     }
