f7182506f71d049208a998f6d4af5ba4ba9b6846
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-07-27 21:57:21 +0100

    Added in changes for file size limit
    Former-commit-id: 504e79e2522167247a3f9462c67dd3ad32efd541

---
+++ src/main/java/uk/gov/hmcts/dm/security/MultipartFileSizeValidator.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.dm.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.multipart.MultipartFile;
+import uk.gov.hmcts.dm.service.FileSizeVerifier;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class MultipartFileSizeValidator
+    implements ConstraintValidator<MultipartFileSizeLimit,List<MultipartFile>> {
+
+    private final FileSizeVerifier fileSizeVerifier;
+
+    @Autowired
+    public MultipartFileSizeValidator(FileSizeVerifier fileSizeVerifier) {
+        this.fileSizeVerifier = fileSizeVerifier;
+    }
+
+
+    @Override
+    public void initialize(MultipartFileSizeLimit fileSizeLimit) {
+    }
+
+    @Override
+    public boolean isValid(List<MultipartFile> multipartFiles, ConstraintValidatorContext context) {
+        return CollectionUtils.isEmpty(multipartFiles) || multipartFiles.stream().allMatch(ft ->
+            fileSizeVerifier.verifyFileSize(ft));
+    }
+
+}
