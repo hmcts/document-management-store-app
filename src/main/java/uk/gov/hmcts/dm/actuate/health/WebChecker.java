448bbb568a44e897f2d21a09c28caa8ca407a89e
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-09-04 11:01:55 +0100

    Resolved Sonar issue

--- src/main/java/uk/gov/hmcts/dm/actuate/health/WebChecker.java
+++ src/main/java/uk/gov/hmcts/dm/actuate/health/WebChecker.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.dm.actuate.health;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.dm.actuate.health.model.HealthCheckResponse;
+
+import java.util.Objects;
 
 public class WebChecker {
 
@@ -30,7 +33,10 @@ public class WebChecker {
     private boolean getStatus() {
         try {
             final HealthCheckResponse healthCheckResponse = restTemplate.getForObject(url + "/health", HealthCheckResponse.class);
-            return "UP".equalsIgnoreCase(healthCheckResponse.getStatus());
+            if (Objects.nonNull(healthCheckResponse) && StringUtils.isNotBlank(healthCheckResponse.getStatus())) {
+                return "UP".equalsIgnoreCase(healthCheckResponse.getStatus());
+            }
+            return false;
         } catch (Exception ex) {
             log.error(name + " " + url + "/health Failed", ex);
             return false;
