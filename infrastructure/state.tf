29b0baec8b521318d35640bcb484c2f859ee21a2
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-11-20 16:58:26 +0000

    Updated azurerm version
    Former-commit-id: 18648943f58d03d932699b62aecacee5858035df

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -2,9 +2,9 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
-          azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 2.35.0"
-          }
-  }
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 2.35"
+      }
+   }
 }
