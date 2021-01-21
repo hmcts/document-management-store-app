6425d857d01351392c0a541d1d9da0786a322085
Raj Katla <raj.katla@hmcts.net>
2021-01-21 18:00:37 +0000

    EM-3317 - Dm-store API - Move large files to remote location and update functional test cases
    Former-commit-id: f4198bbcf81f7476813e32fff59141c4ab11b619

---
+++ src/functionalTest/java/uk/gov/hmcts/dm/functional/DocumentMetadataPropertiesConfig.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.dm.functional;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "large-docs")
+@Component
+public class DocumentMetadataPropertiesConfig {
+
+    private Map<String, DocumentMetadata> metadata = new HashMap<>();
+
+    public Map<String, DocumentMetadata> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, DocumentMetadata> metadata) {
+        this.metadata = metadata;
+    }
+
+    private static class DocumentMetadata {
+        private String documentName;
+        private String extension;
+        private long size;
+
+        public String getDocumentName() {
+            return documentName;
+        }
+
+        public void setDocumentName(String documentName) {
+            this.documentName = documentName;
+        }
+
+        public String getExtension() {
+            return extension;
+        }
+
+        public void setExtension(String extension) {
+            this.extension = extension;
+        }
+
+        public long getSize() {
+            return size;
+        }
+
+        public void setSize(long size) {
+            this.size = size;
+        }
+    }
+
+}
