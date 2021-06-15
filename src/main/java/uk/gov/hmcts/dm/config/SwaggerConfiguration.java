dbbc191322f61f7cc2a5cb99de6ece439f43e755
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-06-15 17:23:31 +0100

    Add swagger endpoint configuration

---
+++ src/main/java/uk/gov/hmcts/dm/config/SwaggerConfiguration.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.dm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import uk.gov.hmcts.dm.controller.StoredDocumentController;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket apiV2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(StoredDocumentController.class.getPackage().getName()))
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiV2Info())
+                .globalRequestParameters(Arrays.asList(
+                        headerServiceAuthorization(),
+                        headerAuthorization()
+                ));
+    }
+
+    private ApiInfo apiV2Info() {
+        return new ApiInfoBuilder()
+                .title("Document Management Store App")
+                .description("download, upload")
+                .version("2-beta")
+                .build();
+    }
+
+    private RequestParameter headerServiceAuthorization() {
+        return new RequestParameterBuilder()
+                .name("ServiceAuthorization")
+                .description("Valid Service-to-Service JWT token for a whitelisted micro-service")
+                .in(ParameterType.HEADER)
+                .required(true)
+                .build();
+    }
+
+    private RequestParameter headerAuthorization() {
+        return new RequestParameterBuilder()
+                .name("Authorization")
+                .description("Keyword `Bearer` followed by a valid IDAM user token")
+                .in(ParameterType.HEADER)
+                .required(true)
+                .build();
+    }
+
+}
