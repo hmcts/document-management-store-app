834fa17793958503859c4260cda253143eb6c13b
rishisharma <rishi.sharma@hmcts.net>
2021-08-31 10:07:10 +0100

    EM-4024: fix terraform version

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -2,9 +2,13 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
-      azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "2.37.0"
-      }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "1.6.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.37.0"
+    }
    }
 }
