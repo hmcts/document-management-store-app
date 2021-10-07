78504cc7c0aecdde199ba26b2aade480d4d6e1b4
rishisharma <rishi.sharma@hmcts.net>
2021-10-07 09:39:12 +0100

    revert last successful nightly build and fix terraform provider issue

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -2,6 +2,10 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
+      azuread = {
+        source  = "hashicorp/azuread"
+        version = "1.6.0"
+      }
       azurerm = {
         source  = "hashicorp/azurerm"
         version = "2.37.0"
