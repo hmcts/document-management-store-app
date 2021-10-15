372bc3b2d8fbd1696790a4a88ddfd32509a608e6
rishisharma <rishi.sharma@hmcts.net>
2021-10-15 11:40:39 +0100

    spring boot upgrade

--- src/smokeTest/groovy/uk/gov/hmcts/dm/smoke/config/SmokeTestContextConfiguration.groovy
+++ src/smokeTest/groovy/uk/gov/hmcts/dm/smoke/config/SmokeTestContextConfiguration.groovy
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 
 @Configuration
-@PropertySource("classpath:application-smoke-test.yaml")
+@PropertySource("classpath:application-smoke-test.properties")
 @ComponentScan("uk.gov.hmcts.dm.smoke")
 class SmokeTestContextConfiguration {
 }
