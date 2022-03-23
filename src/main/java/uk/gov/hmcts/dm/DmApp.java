9883309bd51fe056089f2cc896287bb3b922f51c
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-03-23 16:51:52 +0000

    Reverted com.github.hmcts.liquibase lib changes

--- src/main/java/uk/gov/hmcts/dm/DmApp.java
+++ src/main/java/uk/gov/hmcts/dm/DmApp.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.dm;
 
+import liquibase.configuration.GlobalConfiguration;
+import liquibase.configuration.LiquibaseConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +12,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DmApp {
 
     public static void main(String[] args) {
+        //Setting Liquibase DB Lock property before Spring starts up.
+        LiquibaseConfiguration.getInstance()
+            .getConfiguration(GlobalConfiguration.class)
+            .setUseDbLock(true);
         SpringApplication.run(DmApp.class, args);
     }
 }
