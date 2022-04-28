55a34e20bdb5917b91af9ccb3b1c6a068ed462ae
matt-k1998 <matt.kolakowski@hotmail.com>
2022-04-28 15:50:37 +0100

    Undo change in ApiErrorController

--- src/main/java/uk/gov/hmcts/dm/errorhandler/ApiErrorController.java
+++ src/main/java/uk/gov/hmcts/dm/errorhandler/ApiErrorController.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -20,14 +22,14 @@ public class ApiErrorController extends BasicErrorController {
         super(errorAttributes, new ErrorProperties());
     }
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
     @Override
     public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
         HttpStatus status = getStatus(request);
         if (status == HttpStatus.NO_CONTENT) {
             return new ResponseEntity<>(status);
         }
-
+        Map<String, Object> body = getErrorAttributes(request, getErrorAttributeOptions(request, MediaType.ALL));
         // Set the status again for the legacy error handling code to work
         // This was changed by spring here: https://github.com/spring-projects/spring-boot/commit/a354657acef83919cb3685c7dd50f2a42156efca#r35879840
         // Clearly the error handling in this service is not correct but it's complex enough to take a long time
@@ -35,6 +37,6 @@ public class ApiErrorController extends BasicErrorController {
         // For the sake of time, I've overridden this method to achieve the same functionality as before and raised
         // a Jira ticket as a tech debt item to resolve this in the future.
         status = getStatus(request);
-        return new ResponseEntity<>(status);
+        return new ResponseEntity<>(body, status);
     }
 }
--- src/test/java/uk/gov/hmcts/dm/errorhandler/ApiErrorControllerTest.java
+++ src/test/java/uk/gov/hmcts/dm/errorhandler/ApiErrorControllerTest.java
@@ -3,12 +3,16 @@ package uk.gov.hmcts.dm.errorhandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -24,6 +28,12 @@ public class ApiErrorControllerTest {
     @Mock
     ApiErrorAttributes apiErrorAttributes;
 
+    @Mock
+    WebRequest webRequest;
+
+    @Mock
+    ErrorAttributeOptions errorAttributeOptions;
+
     @Test
     public void shouldReturnResponseEntityNoContent() {
         when(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)).thenReturn(204);
@@ -33,9 +43,10 @@ public class ApiErrorControllerTest {
 
     @Test
     public void shouldReturnResponseEntityInternalServerError() {
+        Map<String, Object> body = apiErrorAttributes.getErrorAttributes(webRequest, errorAttributeOptions);
         when(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)).thenReturn(500);
         ApiErrorController apiErrorController = new ApiErrorController(apiErrorAttributes);
-        assertThat(apiErrorController.error(request), equalTo(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)));
+        assertThat(apiErrorController.error(request), equalTo(new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR)));
     }
 
 }
