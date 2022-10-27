451703b2f8939874973e9768b225d6e634e11d89
mdayican <mustafa.dayican@hmcts.net>
2022-10-27 10:40:34 +0100

    fix checkstyle

--- src/test/java/uk/gov/hmcts/dm/service/BlobStorageDeleteServiceTest.java
+++ src/test/java/uk/gov/hmcts/dm/service/BlobStorageDeleteServiceTest.java
@@ -60,7 +60,8 @@ public class BlobStorageDeleteServiceTest {
             .thenReturn(new BlobsDeleteResponse(null, 202, null, null, null));
         blobStorageDeleteService.deleteDocumentContentVersion(documentContentVersion);
         verify(documentContentVersionRepository, times(1))
-            .updateContentUriAndContentCheckSum(documentContentVersion.getId(), null, null);    }
+            .updateContentUriAndContentCheckSum(documentContentVersion.getId(), null, null);
+    }
 
     @Test
     public void delete_documentContentVersion_if_responseCode_404() {
