453f9724316aaa5149ea821d6a2921253428e437
SomeshRA <somesh.acharya1@hmcts.net>
2022-11-24 17:26:54 +0000

    EM-4832: Updating flyway db to 9.8.2

--- src/main/java/uk/gov/hmcts/dm/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/dm/data/migration/FlywayNoOpStrategy.java
@@ -5,15 +5,20 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
+import static org.flywaydb.core.api.MigrationState.BASELINE;
+import static org.flywaydb.core.api.MigrationState.BASELINE_IGNORED;
+
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
 
     @Override
     public void migrate(Flyway flyway) {
         Stream.of(flyway.info().all())
-            .filter(info -> !info.getState().isApplied())
-            .findFirst()
-            .ifPresent(info -> {
-                throw new PendingMigrationScriptException(info.getScript());
-            });
+                .filter(info -> !BASELINE.equals(info.getState())
+                        && !BASELINE_IGNORED.equals(info.getState())
+                        && !info.getState().isApplied())
+                .findFirst()
+                .ifPresent(info -> {
+                    throw new PendingMigrationScriptException(info.getScript());
+                });
     }
 }
