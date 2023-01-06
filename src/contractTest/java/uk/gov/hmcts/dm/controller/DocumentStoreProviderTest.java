872e0618a0b9d62c69798a833572edcc6739698c
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-01-06 17:07:52 +0000

    temp

--- src/contractTest/java/uk/gov/hmcts/dm/controller/DocumentStoreProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/DocumentStoreProviderTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.dm.controller;
 
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
-import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
