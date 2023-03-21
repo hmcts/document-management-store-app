0bdba03854787b007f7d4a5395d9a18d5c31c3b2
mdayican <mustafa.dayican@hmcts.net>
2023-03-21 12:04:22 +0000

    sanitise log

--- src/main/java/uk/gov/hmcts/dm/service/FileContentVerifier.java
+++ src/main/java/uk/gov/hmcts/dm/service/FileContentVerifier.java
@@ -16,6 +16,8 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Locale;
 
+import static uk.gov.hmcts.dm.utils.StringUtils.sanitiseFileName;
+
 @Service
 @SuppressWarnings({"squid:S2629"})
 public class FileContentVerifier {
@@ -44,7 +46,7 @@ public class FileContentVerifier {
 
         String fileNameExtension = getOriginalFileNameExtension(multipartFile);
         if (!extensionsList.stream().anyMatch(ext -> ext.equalsIgnoreCase(fileNameExtension))) {
-            log.info("Warning. The extension of uploaded file is not white-listed {}", fileNameExtension);
+            log.info("Warning. The extension of uploaded file is not white-listed {}", sanitiseFileName(fileNameExtension));
             return false;
         }
 
