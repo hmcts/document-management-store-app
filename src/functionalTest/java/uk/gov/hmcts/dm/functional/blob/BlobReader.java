f940cacf0b32f3a6aebbef0afb5c33a23015f2a4
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-06-06 13:51:59 +0100

    remove conditionalonbean

--- src/functionalTest/java/uk/gov/hmcts/dm/functional/blob/BlobReader.java
+++ src/functionalTest/java/uk/gov/hmcts/dm/functional/blob/BlobReader.java
@@ -5,11 +5,11 @@ import com.azure.storage.blob.specialized.BlockBlobClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnBean(BlobServiceClient.class)
+//@ConditionalOnBean(BlobServiceClient.class)
 public class BlobReader {
 
     private static final Logger LOG = LoggerFactory.getLogger(BlobReader.class);
