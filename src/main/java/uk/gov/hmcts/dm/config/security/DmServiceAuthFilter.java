c7ba6e5875a277b7cdc34d18f2032cf2b8cd4579
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-07-12 17:45:13 +0100

    chekstyle

--- src/main/java/uk/gov/hmcts/dm/config/security/DmServiceAuthFilter.java
+++ src/main/java/uk/gov/hmcts/dm/config/security/DmServiceAuthFilter.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.dm.config.security;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
