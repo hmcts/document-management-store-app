78a2dd4bd1a3fe274b1381a4b1b27a9340072640
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2023-07-24 12:03:00 +0100

    Updated Dao related classes

--- src/main/java/uk/gov/hmcts/dm/repository/DocumentDaoImpl.java
+++ src/main/java/uk/gov/hmcts/dm/repository/DocumentDaoImpl.java
@@ -3,11 +3,16 @@ package uk.gov.hmcts.dm.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import uk.gov.hmcts.dm.domain.AuditActions;
 import uk.gov.hmcts.dm.domain.DocumentContentVersion;
 import uk.gov.hmcts.dm.domain.StoredDocument;
-import uk.gov.hmcts.dm.domain.mapper.DocumentContentVersionMapper;
+import uk.gov.hmcts.dm.domain.StoredDocumentAuditEntry;
 import uk.gov.hmcts.dm.domain.mapper.StoredDocumentMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -21,14 +26,65 @@ public class DocumentDaoImpl {
         String query = "SELECT * FROM STOREDDOCUMENT WHERE ID = ?";
         StoredDocument storedDocument = jdbcTemplate.queryForObject(
             query, new Object[] { id }, new StoredDocumentMapper());
+        //Below will require an Index to added for the storeddocument_id column
+        List<DocumentContentVersion> documentContentVersions = getDocumentContentVersions(storedDocument.getId());
+        storedDocument.setDocumentContentVersions(documentContentVersions);
+        documentContentVersions.forEach(
+            documentContentVersion -> documentContentVersion.setStoredDocument(storedDocument)
+        );
+        //Below will require an Index to added for the storeddocument_id column
+        List<StoredDocumentAuditEntry> storedDocumentAuditEntries =
+            getStoredDocumentAuditEntries(storedDocument.getId());
+        storedDocument.setAuditEntries(new HashSet<>(storedDocumentAuditEntries));
+        storedDocumentAuditEntries.forEach(
+            storedDocumentAuditEntry -> storedDocumentAuditEntry.setStoredDocument(storedDocument)
+        );
         return storedDocument;
     }
 
-    public DocumentContentVersion getDocumentContentVersion(UUID id) {
+    public List<DocumentContentVersion> getDocumentContentVersions(UUID id) {
 
-        String query = "SELECT * FROM STOREDDOCUMENT WHERE ID = ?";
-        DocumentContentVersion documentContentVersion = jdbcTemplate.queryForObject(
-            query, new Object[] { id }, new DocumentContentVersionMapper());
-        return documentContentVersion;
+        String sql = "SELECT * FROM DocumentContentVersion WHERE storeddocument_id = ?";
+        List<DocumentContentVersion> documentContentVersions = jdbcTemplate.query(
+            sql, new Object[]{id},
+            (rs, rowNum) -> new DocumentContentVersion(
+                UUID.fromString(rs.getString("id")),
+                rs.getString("mimeType"),
+                rs.getString("originalDocumentName"),
+                rs.getString("createdBy"),
+                rs.getString("createdByService"),
+                rs.getDate("createdOn"),
+                null,
+                null,
+                rs.getLong("size"),
+                rs.getString("content_uri"),
+                rs.getString("content_checksum")
+            ));
+
+        return documentContentVersions;
+    }
+
+    public List<StoredDocumentAuditEntry> getStoredDocumentAuditEntries(UUID id) {
+
+        String sql = "SELECT * FROM AuditEntry WHERE storeddocument_id = ?";
+        List<StoredDocumentAuditEntry> StoredDocumentAuditEntries = jdbcTemplate.query(
+            sql, new Object[]{id},
+            (rs, rowNum) -> createAndSaveStoredDocumentAuditEntry(rs));
+
+        return StoredDocumentAuditEntries;
+    }
+
+    private StoredDocumentAuditEntry createAndSaveStoredDocumentAuditEntry(ResultSet rs) {
+        StoredDocumentAuditEntry storedDocumentAuditEntry = new StoredDocumentAuditEntry();
+        try {
+            storedDocumentAuditEntry.setId(UUID.fromString(rs.getString("id")));
+            storedDocumentAuditEntry.setAction(AuditActions.valueOf(rs.getString("action")));
+            storedDocumentAuditEntry.setUsername(rs.getString("username"));
+            storedDocumentAuditEntry.setServiceName(rs.getString("servicename"));
+            storedDocumentAuditEntry.setRecordedDateTime(rs.getDate("recordeddatetime"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return storedDocumentAuditEntry;
     }
 }
--- src/main/java/uk/gov/hmcts/dm/service/DocumentContentVersionService.java
+++ src/main/java/uk/gov/hmcts/dm/service/DocumentContentVersionService.java
@@ -1,17 +1,18 @@
 package uk.gov.hmcts.dm.service;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.dm.domain.DocumentContentVersion;
 import uk.gov.hmcts.dm.domain.StoredDocument;
 import uk.gov.hmcts.dm.repository.DocumentContentVersionRepository;
+import uk.gov.hmcts.dm.repository.DocumentDaoImpl;
 import uk.gov.hmcts.dm.repository.StoredDocumentRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Transactional
 @Service
 public class DocumentContentVersionService {
 
@@ -21,14 +22,31 @@ public class DocumentContentVersionService {
     @Autowired
     private StoredDocumentRepository storedDocumentRepository;
 
+    @Autowired
+    private DocumentDaoImpl documentDao;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     public Optional<DocumentContentVersion> findById(UUID id) {
         return documentContentVersionRepository.findById(id);
     }
 
     public Optional<DocumentContentVersion> findMostRecentDocumentContentVersionByStoredDocumentId(UUID id) {
-        return storedDocumentRepository
-                    .findByIdAndDeleted(id, false)
-                    .map(StoredDocument::getMostRecentDocumentContentVersion);
+        HikariDataSource dataSource = (HikariDataSource) jdbcTemplate.getDataSource();
+        System.out.println("Active in DocumentContentVersionService : " + dataSource.getHikariPoolMXBean().getActiveConnections());
+        System.out.println("Idle in DocumentContentVersionService : " + dataSource.getHikariPoolMXBean().getIdleConnections());
+//        Optional<DocumentContentVersion> documentContentVersion = storedDocumentRepository
+//                    .findByIdAndDeleted(id, false)
+//                    .map(StoredDocument::getMostRecentDocumentContentVersion);
+
+        StoredDocument storedDocument = documentDao.getStoredDocument(id);
+
+        DocumentContentVersion documentContentVersion = storedDocument.getMostRecentDocumentContentVersion();
+
+
+        System.out.println("After : " + dataSource.getHikariPoolMXBean().getActiveConnections());
+        return null;
     }
 
 }
