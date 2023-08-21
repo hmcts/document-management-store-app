ce0c7ca7aada3d29295d0a5c237ae4458d3f398d
Jefex7 <duartepireslopes@gmail.com>
2023-08-21 11:33:37 +0100

    Returning to deprecated Method

--- src/main/java/uk/gov/hmcts/dm/config/security/SpringSecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/security/SpringSecurityConfiguration.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.dm.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -13,7 +13,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity (prePostEnabled = true)
 public class SpringSecurityConfiguration {
 
     private DmServiceAuthFilter dmServiceAuthFilter;
