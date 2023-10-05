3eb87f904060f900cbf58ad44ae19afd12acba92
Jefex7 <duartepireslopes@gmail.com>
2023-10-05 09:17:04 +0100

    removing Telemetry Configuration

--- src/main/java/uk/gov/hmcts/dm/appinsights/AppInsightsEventRepository.java
+++ src/main/java/uk/gov/hmcts/dm/appinsights/AppInsightsEventRepository.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.dm.appinsights;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import com.microsoft.applicationinsights.TelemetryConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -14,9 +12,7 @@ public class AppInsightsEventRepository implements EventRepository {
     private final TelemetryClient telemetry;
 
     @Autowired
-    public AppInsightsEventRepository(@Value("${azure.application-insights.instrumentation-key}")
-                                          String instrumentationKey, TelemetryClient telemetry) {
-        TelemetryConfiguration.getActive().setInstrumentationKey(instrumentationKey);
+    public AppInsightsEventRepository(TelemetryClient telemetry) {
         telemetry.getContext().getComponent().setVersion(getClass().getPackage().getImplementationVersion());
         this.telemetry = telemetry;
     }
