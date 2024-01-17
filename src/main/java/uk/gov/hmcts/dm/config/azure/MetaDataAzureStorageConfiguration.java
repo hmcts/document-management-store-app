4e07d4a968c7bb7ca11fa56cbc0f3cc9c51a0b54
Linus Norton <linusnorton@gmail.com>
2024-01-17 12:07:57 +0000

    clean up

--- src/main/java/uk/gov/hmcts/dm/config/azure/MetaDataAzureStorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/azure/MetaDataAzureStorageConfiguration.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.dm.config.azure;
 
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
-import com.azure.storage.blob.models.BlobStorageException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
