d9eabc7ac2b1cad1b31c656cdbe503cc0edd9874
lars lunde <lars.lunde@hmcts.net>
2024-07-18 10:40:42 +0100

    Created PDFPasswordCheckService, test class and test data

---
+++ src/main/java/uk/gov/hmcts/dm/service/PDFPasswordCheckService.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.dm.service;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PDFPasswordCheckService {
+
+    private static final Logger log = LoggerFactory.getLogger(PDFPasswordCheckService.class);
+
+    public static void checkPasswordProtectedPDF(String filePath) {
+        File file = new File(filePath);
+        try (PDDocument document = Loader.loadPDF(file)) {
+            log.info("The document {} is not password protected.", file.getName());
+        } catch (InvalidPasswordException e) {
+            log.error("The document {} is password protected.", file.getName());
+        } catch (IOException e) {
+            log.error("An error occurred while trying to load the document {}.", file.getName());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/dm/service/PDFPasswordCheckServiceTest.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.dm.service;
+
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PDFPasswordCheckServiceTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"src/test/resources/files/not_pw_protected.pdf",
+                            "src/test/resources/files/pw_protected.pdf",
+                            "src/test/resources/files/non_existent.pdf",
+                            "invalid/path/to/pdf.pdf"})
+
+    void shouldCheckFileForPasswordProtection(String input) {
+        PDFPasswordCheckService.checkPasswordProtectedPDF(input);
+    }
+    @Test
+    public void shouldLogErrorWhenNoFilePathProvided() {
+        String path = null;
+        PDFPasswordCheckService.checkPasswordProtectedPDF(path);
+    }
+}
