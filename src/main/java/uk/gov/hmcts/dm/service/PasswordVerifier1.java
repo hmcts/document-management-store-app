b5bda1085ccbc11890649258eeb752ee7e7eb775
lars lunde <lars.lunde@hmcts.net>
2024-08-09 09:23:22 +0100

    Additional file formats added

--- src/main/java/uk/gov/hmcts/dm/service/PasswordVerifier1.java
+++ src/main/java/uk/gov/hmcts/dm/service/PasswordVerifier1.java
@@ -1,33 +1,104 @@
 package uk.gov.hmcts.dm.service;
 
-import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.commons.csv.CSVParser;
 import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.parser.microsoft.ooxml.OOXMLParser;
+import org.apache.tika.parser.txt.TXTParser;
+import org.apache.tika.parser.image.JpegParser;
+import org.apache.tika.parser.image.TiffParser;
+import org.apache.tika.parser.image.ImageParser;
+import org.apache.tika.parser.csv.TextAndCSVParser;
+import org.apache.tika.parser.mp4.MP4Parser;
+import org.apache.tika.parser.odf.OpenDocumentParser;
+import org.apache.tika.parser.audio.MidiParser;
+
+//import org.apache.tika.parser.rtf.RTFParser;
+
 import org.apache.tika.sax.BodyContentHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.SAXException;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class PasswordVerifier1 {
 
     public boolean checkPasswordProtectedFile(MultipartFile multipartFile) throws IOException {
 
+         String ODF_formats =
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet," +
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.template," +
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document," +
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.template," +
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation," +
+            "application/vnd.openxmlformats-officedocument.presentationml.template," +
+            "application/vnd.openxmlformats-officedocument.presentationml.slideshow,";
+
         InputStream inputStream = new BufferedInputStream(multipartFile.getInputStream());
         String documentType = detectDocTypeUsingFacade(inputStream);
-        BodyContentHandler handler = new BodyContentHandler();
+
+        PDFParser pdfparser = new PDFParser();
+        OOXMLParser ooxmlparser = new OOXMLParser();
+        TXTParser  textParser = new TXTParser();
+        TextAndCSVParser textAndCSVParser = new TextAndCSVParser();
+        JpegParser jpegParser = new JpegParser();
+        TiffParser tiffParser = new TiffParser();
+        ImageParser imageParser = new ImageParser();
+        MP4Parser mp4Parser = new MP4Parser();
+        MidiParser midiParser = new MidiParser();
+        OpenDocumentParser openDocumentParser = new OpenDocumentParser();
+
 
         switch (documentType) {
             case "application/pdf":
-                return checkPdfPassword(inputStream);
+                return checkPasswordWithParser(inputStream, pdfparser);
+            case "application/vnd.ms-excel | application/msword | application/vnd.ms-powerpoint":
+                return checkPasswordWithParser(inputStream, ooxmlparser);
             case "text/plain":
-                return true;
+                return checkPasswordWithParser(inputStream, textParser);
+            case "text/csv":
+                return checkPasswordWithParser(inputStream, textAndCSVParser);
+            case "image/jpeg,":
+                return checkPasswordWithParser(inputStream, jpegParser);
+            case "image/tiff":
+                return checkPasswordWithParser(inputStream, tiffParser);
+            case "image/png | image/bmp": //NOT SURE IF THE imageParser is the right one
+                return checkPasswordWithParser(inputStream, imageParser);
+            case "audio/mp4 | video/mp4}":
+                return checkPasswordWithParser(inputStream, mp4Parser);
+            case "audio/mpeg":
+                return checkPasswordWithParser(inputStream, midiParser);
+            case ODF_formats:
+                return checkPasswordWithParser(inputStream, openDocumentParser);
+            case "test":
+                return checkPasswordWithParser(inputStream, ?);
+            case "test":
+                return checkPasswordWithParser(inputStream, ?);
             default:
                 return false;
         }
+    }
+
+    private boolean checkPasswordWithParser(InputStream inputStream, Parser parser) {
+        try {
+            BodyContentHandler handler = new BodyContentHandler();
+            Metadata metadata = new Metadata();
+            ParseContext pcontext = new ParseContext();
+
+            parser.parse(inputStream, handler, metadata, pcontext);
+
+        } catch (TikaException | IOException | SAXException e) {
+            return false;
+        }
+        return true;
     }
 
     private boolean checkPdfPassword(InputStream inputStream) {
@@ -37,12 +108,28 @@ public class PasswordVerifier1 {
             ParseContext pcontext = new ParseContext();
 
             PDFParser pdfparser = new PDFParser();
-            pdfparser.parse(inputStream, handler, metadata,pcontext);
-        } catch (TikaException | IOException e) {
+            pdfparser.parse(inputStream, handler, metadata, pcontext);
+        } catch (TikaException | IOException | SAXException e) {
                 return false;
             }
         return true;
     }
+
+    private boolean checkMicrosoftSuitePassword(InputStream inputStream) {
+        try {
+            BodyContentHandler handler = new BodyContentHandler();
+            Metadata metadata = new Metadata();
+            ParseContext pcontext = new ParseContext();
+
+            OOXMLParser ooxmlparser = new OOXMLParser();
+            ooxmlparser.parse(inputStream, handler, metadata, pcontext);
+        } catch (TikaException | IOException | SAXException e) {
+            return false;
+        }
+        return true;
+    }
+
+
 
     private static String detectDocTypeUsingFacade(InputStream stream)
         throws IOException {
