1d9be37e1010e85a00dd834b7bd0823b5d73c8a0
lars lunde <lars.lunde@hmcts.net>
2024-08-20 14:55:22 +0100

    Test changes for code coverage, no files included for test. Commented tests currently broken (.xls). mp4 video resolves as audio/mp4 and csv resolves as text/plain.

--- src/main/java/uk/gov/hmcts/dm/service/PasswordVerifier.java
+++ src/main/java/uk/gov/hmcts/dm/service/PasswordVerifier.java
@@ -1,8 +1,13 @@
 package uk.gov.hmcts.dm.service;
 
 import org.apache.tika.Tika;
+import org.apache.tika.detect.Detector;
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.mime.MimeTypes;
+import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.audio.MidiParser;
@@ -25,29 +30,36 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+
+
 @Service
 public class PasswordVerifier {
 
-    private static final String ODF_FORMAT =
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,"
-        + "application/vnd.openxmlformats-officedocument.spreadsheetml.template,"
-        + "application/vnd.openxmlformats-officedocument.wordprocessingml.document,"
-        + "application/vnd.openxmlformats-officedocument.wordprocessingml.template,"
-        + "application/vnd.openxmlformats-officedocument.presentationml.presentation,"
-        + "application/vnd.openxmlformats-officedocument.presentationml.template,"
-        + "application/vnd.openxmlformats-officedocument.presentationml.slideshow";
-
+    private static final String OPENXML_SHEET =
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    private static final String OPENXML_SHEET_TEMPLATE =
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.template";
+    private static final String OPENXML_DOC =
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+    private static final String OPENXML_DOC_TEMPLATE =
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.template";
+    private static final String OPENXML_PRESENTATION =
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+    private static final String OPENXML_PRESENTATION_TEMPLATE =
+        "application/vnd.openxmlformats-officedocument.presentationml.template";
+    private static final String OPENXML_PRESENTATION_SLIDESHOW =
+        "application/vnd.openxmlformats-officedocument.presentationml.slideshow";
 
     public boolean checkPasswordProtectedFile(MultipartFile multipartFile) {
 
-        String documentType;
         InputStream inputStream;
         try {
             inputStream = new BufferedInputStream(multipartFile.getInputStream());
-            documentType = detectDocTypeUsingFacade(inputStream);
         } catch (IOException e) {
             return false;
         }
+
+        String mimeType = getRealMimeType(multipartFile);
 
         PDFParser pdfparser = new PDFParser();
         OOXMLParser ooxmlparser = new OOXMLParser();
@@ -61,26 +73,32 @@ public class PasswordVerifier {
         OpenDocumentParser openDocumentParser = new OpenDocumentParser();
         RTFParser rtfParser = new RTFParser();
 
-        return switch (documentType) {
+        return switch (mimeType) {
             case "application/pdf" -> checkPasswordWithParser(inputStream, pdfparser);
-            case "application/vnd.ms-excel | application/msword | application/vnd.ms-powerpoint" ->
-                    checkPasswordWithParser(inputStream, ooxmlparser);
-            case "text/plain" -> checkPasswordWithParser(inputStream, textParser);
+            case "application/vnd.ms-excel", "application/msword", "application/vnd.ms-powerpoint" ->
+                checkPasswordWithParser(inputStream, ooxmlparser);
             case "text/csv" -> checkPasswordWithParser(inputStream, textAndCSVParser);
-            case "image/jpeg," -> checkPasswordWithParser(inputStream, jpegParser);
+            case "text/plain" -> checkPasswordWithParser(inputStream, textParser);
+            case "image/jpeg" -> checkPasswordWithParser(inputStream, jpegParser);
             case "image/tiff" -> checkPasswordWithParser(inputStream, tiffParser);
-            case "image/png | image/bmp" ->
-                    checkPasswordWithParser(inputStream, imageParser); //NOT SURE IF THE imageParser is the right one
-            case "audio/mp4 | video/mp4}" -> checkPasswordWithParser(inputStream, mp4Parser);
+            case "image/png", "image/bmp" -> checkPasswordWithParser(inputStream, imageParser);
+            case "audio/mp4", "video/mp4" -> checkPasswordWithParser(inputStream, mp4Parser);
             case "audio/mpeg" -> checkPasswordWithParser(inputStream, midiParser);
-            case ODF_FORMAT -> checkPasswordWithParser(inputStream, openDocumentParser);
+            //case ODF_FORMAT -> checkPasswordWithParser(inputStream, openDocumentParser);
             case "application/rtf" -> checkPasswordWithParser(inputStream, rtfParser);
             case "application/octect-stream" -> true; //Change to make correct call for format.
+            case OPENXML_SHEET -> checkPasswordWithParser(inputStream, openDocumentParser);
+            case OPENXML_SHEET_TEMPLATE -> checkPasswordWithParser(inputStream, openDocumentParser);
+            case OPENXML_DOC -> checkPasswordWithParser(inputStream, openDocumentParser);
+            case OPENXML_DOC_TEMPLATE -> checkPasswordWithParser(inputStream, openDocumentParser);
+            case OPENXML_PRESENTATION -> checkPasswordWithParser(inputStream, openDocumentParser);
+            case OPENXML_PRESENTATION_TEMPLATE -> checkPasswordWithParser(inputStream, openDocumentParser);
+            case OPENXML_PRESENTATION_SLIDESHOW -> checkPasswordWithParser(inputStream, openDocumentParser);
             default -> false;
         };
     }
 
-    private boolean checkPasswordWithParser(InputStream inputStream, Parser parser) {
+    public boolean checkPasswordWithParser(InputStream inputStream, Parser parser) {
         try {
             BodyContentHandler handler = new BodyContentHandler();
             Metadata metadata = new Metadata();
@@ -94,10 +112,16 @@ public class PasswordVerifier {
         return true;
     }
 
-    private static String detectDocTypeUsingFacade(InputStream stream)
-        throws IOException {
-
-        Tika tika = new Tika();
-        return tika.detect(stream);
+    public static String getRealMimeType(MultipartFile file) {
+        AutoDetectParser parser = new AutoDetectParser();
+        Detector detector = parser.getDetector();
+        try {
+            Metadata metadata = new Metadata();
+            TikaInputStream stream = TikaInputStream.get(file.getInputStream());
+            MediaType mediaType = detector.detect(stream, metadata);
+            return mediaType.toString();
+        } catch (IOException e) {
+            return MimeTypes.OCTET_STREAM;
+        }
     }
 }
--- src/test/java/uk/gov/hmcts/dm/service/PasswordVerifierTest.java
+++ src/test/java/uk/gov/hmcts/dm/service/PasswordVerifierTest.java
@@ -6,14 +6,34 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 class PasswordVerifierTest {
+
+    private static final String OPENXML_DOC =
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+    private static final String OPENXML_DOTX =
+        "filename.dotx, application/vnd.openxmlformats-officedocument.wordprocessingml.template";
+    private static final String OPENXML_SHEET =
+        "filename.xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    private static final String OPENXML_SHEET_TEMPLATE =
+        "filename.xltx, application/vnd.openxmlformats-officedocument.spreadsheetml.template";
+    private static final String OPENXML_PRESENTATION =
+        "filename.pptx, application/vnd.openxmlformats-officedocument.presentationml.presentation";
+    private static final String OPENXML_PRESENTATION_TEMPLATE =
+        "filename.potx, application/vnd.openxmlformats-officedocument.presentationml.template";
+    private static final String OPENXML_PRESENTATION_SLIDESHOW =
+        "filename.ppsx, application/vnd.openxmlformats-officedocument.presentationml.slideshow";
 
     @InjectMocks
     PasswordVerifier passwordVerifier;
@@ -21,22 +41,6 @@ class PasswordVerifierTest {
     @BeforeEach
     void init() {
         MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void verifyPassword_PDF() {
-        MultipartFile file =
-            new MockMultipartFile("files", "file.pdf", "application/pdf", "hello".getBytes(StandardCharsets.UTF_8));
-
-        assertTrue(passwordVerifier.checkPasswordProtectedFile(file));
-    }
-
-    @Test
-    void verifyPassword_CSV() {
-        MultipartFile file =
-            new MockMultipartFile("files", "file.csv", "text/csv", "hello".getBytes(StandardCharsets.UTF_8));
-
-        assertTrue(passwordVerifier.checkPasswordProtectedFile(file));
     }
 
     @ParameterizedTest
@@ -62,10 +66,174 @@ class PasswordVerifierTest {
         "filename.xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         "filename.xltx, application/vnd.openxmlformats-officedocument.spreadsheetml.template"
     })
-    void testPasswordVerifierSuccess_parameterized(String filename, String mimetype) {
+    void testPasswordVerifier_parameterized_success(String filename, String mimetype) {
         MultipartFile file =
             new MockMultipartFile("files", filename, mimetype, "hello".getBytes(StandardCharsets.UTF_8));
 
         assertTrue(passwordVerifier.checkPasswordProtectedFile(file));
+    }
+
+
+    @Test
+    void testPasswordVerifier_pdf() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/test.pdf").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.pdf", "application/pdf", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_docx_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/test.docx").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.docx", OPENXML_DOC, inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_docx_failure() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/pw_protected_docx.docx").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.docx", OPENXML_DOC, inputStream);
+
+        assertFalse(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_dotx_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.dotx").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.dotx", OPENXML_DOTX, inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_pptx_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.pptx").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.dotx", OPENXML_PRESENTATION, inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_potx_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.potx").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.dotx", OPENXML_PRESENTATION_TEMPLATE, inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_ppsx_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.ppsx").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.dotx", OPENXML_PRESENTATION_SLIDESHOW, inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    //    @Test
+    //    void testPasswordVerifier_xls_success() throws IOException {
+    //        InputStream inputStream = new ClassPathResource("files/file.xls").getInputStream();
+    //        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+    //        "file", "sample.xls", OPENXML_SHEET, inputStream);
+    //
+    //        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    //    }
+
+    @Test
+    void testPasswordVerifier_xltx_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.xltx").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.xltx", OPENXML_SHEET_TEMPLATE, inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_jpeg_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.jpg").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.jpg", "application/jpeg", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_png_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.png").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.png", "application/png", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_tif_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.tif").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.tif", "image/tiff", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_bmp_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.bmp").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.bmp", "image/bmp", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_mp3_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/audio_test.mp3").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.mp3", "audio/mpeg", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    //TODO: resolves as audio/mp4
+    @Test
+    void testPasswordVerifier_mp4_video_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.mp4").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.mp4", "video/mp4", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_mp4_audio_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.mp4").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.mp4", "audio/mp4", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_txt_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.txt").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.txt", "text/plain", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    @Test
+    void testPasswordVerifier_rtf_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.rtf").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+            "file", "sample.rtf", "application/rtf", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
+    }
+
+    //TODO: CSV test resolves to text/plain still
+    @Test
+    void testPasswordVerifier_csv_success() throws IOException {
+        InputStream inputStream = new ClassPathResource("files/file.csv").getInputStream();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "sample.csv", "text/csv", inputStream);
+
+        assertTrue(passwordVerifier.checkPasswordProtectedFile(mockMultipartFile));
     }
 }
