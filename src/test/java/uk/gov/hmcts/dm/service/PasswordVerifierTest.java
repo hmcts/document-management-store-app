fe47bbb58034ffc633f9df4a738534addfd7aec7
lars lunde <lars.lunde@hmcts.net>
2024-08-29 15:31:09 +0100

    Fixed imports

--- src/test/java/uk/gov/hmcts/dm/service/PasswordVerifierTest.java
+++ src/test/java/uk/gov/hmcts/dm/service/PasswordVerifierTest.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.*;
 
