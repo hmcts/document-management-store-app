36e82219c8552e2f0ce9c147d090b46deb14815f
mdayican <mdayican@gmail.com>
2024-12-27 13:25:03 +0000

    EM-6347 junit5 upgrade part-4

--- src/test/java/uk/gov/hmcts/dm/service/StoredDocumentServiceTests.java
+++ src/test/java/uk/gov/hmcts/dm/service/StoredDocumentServiceTests.java
@@ -1,14 +1,13 @@
 package uk.gov.hmcts.dm.service;
 
 import org.assertj.core.util.Maps;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.dm.commandobject.DocumentUpdate;
 import uk.gov.hmcts.dm.commandobject.UpdateDocumentCommand;
 import uk.gov.hmcts.dm.commandobject.UpdateDocumentsCommand;
@@ -36,6 +35,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
@@ -47,8 +49,8 @@ import static uk.gov.hmcts.dm.componenttests.TestUtil.STORED_DOCUMENT;
 import static uk.gov.hmcts.dm.componenttests.TestUtil.TEST_FILE;
 import static uk.gov.hmcts.dm.security.Classifications.PRIVATE;
 
-@RunWith(SpringRunner.class)
-public class StoredDocumentServiceTests {
+@ExtendWith(SpringExtension.class)
+class StoredDocumentServiceTests {
 
     @Mock
     private StoredDocumentRepository storedDocumentRepository;
@@ -71,62 +73,62 @@ public class StoredDocumentServiceTests {
     @InjectMocks
     private StoredDocumentService storedDocumentService;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         when(securityUtilService.getUserId()).thenReturn("Corín Tellado");
     }
 
     @Test
-    public void testFindOne() {
+    void testFindOne() {
         when(this.storedDocumentRepository.findById(any(UUID.class))).thenReturn(Optional.of(STORED_DOCUMENT));
         Optional<StoredDocument> storedDocument = storedDocumentService.findOne(TestUtil.RANDOM_UUID);
         assertThat(storedDocument.get(), equalTo(STORED_DOCUMENT));
     }
 
     @Test
-    public void testFindOneThatDoesNotExist() {
+    void testFindOneThatDoesNotExist() {
         when(this.storedDocumentRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
         Optional<StoredDocument> storedDocument = storedDocumentService.findOne(TestUtil.RANDOM_UUID);
         assertFalse(storedDocument.isPresent());
     }
 
     @Test
-    public void testFindOneThatIsMarkedDeleted() {
+    void testFindOneThatIsMarkedDeleted() {
         when(this.storedDocumentRepository.findById(any(UUID.class))).thenReturn(Optional.of(DELETED_DOCUMENT));
         Optional<StoredDocument> storedDocument = storedDocumentService.findOne(TestUtil.RANDOM_UUID);
         assertFalse(storedDocument.isPresent());
     }
 
     @Test
-    public void testFindOneWithBinaryDataThatDoesNotExist() {
+    void testFindOneWithBinaryDataThatDoesNotExist() {
         when(this.storedDocumentRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
         Optional<StoredDocument> storedDocument = storedDocumentService.findOneWithBinaryData(TestUtil.RANDOM_UUID);
         assertFalse(storedDocument.isPresent());
     }
 
     @Test
-    public void testFindOneWithBinaryDataThatIsMarkedHardDeleted() {
+    void testFindOneWithBinaryDataThatIsMarkedHardDeleted() {
         when(this.storedDocumentRepository.findById(any(UUID.class))).thenReturn(Optional.of(HARD_DELETED_DOCUMENT));
         Optional<StoredDocument> storedDocument = storedDocumentService.findOneWithBinaryData(TestUtil.RANDOM_UUID);
         assertFalse(storedDocument.isPresent());
     }
 
     @Test
-    public void testFindOneWithBinaryDataThatIsMarkedDeleted() {
+    void testFindOneWithBinaryDataThatIsMarkedDeleted() {
         when(this.storedDocumentRepository.findById(any(UUID.class))).thenReturn(Optional.of(DELETED_DOCUMENT));
         Optional<StoredDocument> storedDocument = storedDocumentService.findOneWithBinaryData(TestUtil.RANDOM_UUID);
         assertTrue(storedDocument.isPresent());
     }
 
     @Test
-    public void testSave() {
+    void testSave() {
         final StoredDocument storedDocument = STORED_DOCUMENT;
         storedDocumentService.save(storedDocument);
         verify(storedDocumentRepository).save(storedDocument);
     }
 
     @Test
-    public void testSaveItemsWithCommand() {
+    void testSaveItemsWithCommand() {
         UploadDocumentsCommand uploadDocumentsCommand = new UploadDocumentsCommand();
         uploadDocumentsCommand.setFiles(singletonList(TEST_FILE));
         uploadDocumentsCommand.setRoles(List.of("a", "b"));
@@ -143,14 +145,14 @@ public class StoredDocumentServiceTests {
         assertEquals(1, documents.size());
         assertEquals(storedDocument.getRoles(), newHashSet("a", "b"));
         assertEquals(PRIVATE, storedDocument.getClassification());
-        Assert.assertNull(storedDocument.getMetadata());
-        Assert.assertNotNull(storedDocument.getTtl());
+        assertNull(storedDocument.getMetadata());
+        assertNotNull(storedDocument.getTtl());
         assertEquals(TEST_FILE.getContentType(), latestVersion.getMimeType());
         assertEquals(TEST_FILE.getOriginalFilename(), latestVersion.getOriginalDocumentName());
     }
 
     @Test
-    public void testSaveItemsWithCommandAndToggleConfiguration() {
+    void testSaveItemsWithCommandAndToggleConfiguration() {
 
         when(toggleConfiguration.isMetadatasearchendpoint()).thenReturn(true);
 
@@ -170,13 +172,13 @@ public class StoredDocumentServiceTests {
         assertEquals(storedDocument.getRoles(), newHashSet("a", "b"));
         assertEquals(PRIVATE, storedDocument.getClassification());
         assertEquals(storedDocument.getMetadata(), Map.of("prop1", "value1"));
-        Assert.assertNotNull(storedDocument.getTtl());
+        assertNotNull(storedDocument.getTtl());
         assertEquals(TEST_FILE.getContentType(), latestVersion.getMimeType());
         assertEquals(TEST_FILE.getOriginalFilename(), latestVersion.getOriginalDocumentName());
     }
 
     @Test
-    public void testSaveItemsToAzure() {
+    void testSaveItemsToAzure() {
         List<StoredDocument> documents = storedDocumentService.saveItems(singletonList(TEST_FILE));
 
         assertEquals(1, documents.size());
@@ -189,7 +191,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testAddStoredDocumentVersionWhenAzureBlobStoreEnabled() {
+    void testAddStoredDocumentVersionWhenAzureBlobStoreEnabled() {
 
         StoredDocument storedDocument = new StoredDocument();
 
@@ -210,7 +212,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testDelete() {
+    void testDelete() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocumentService.deleteDocument(storedDocument, false);
 
@@ -219,7 +221,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testHardDeleteAzureBlobEnabled() {
+    void testHardDeleteAzureBlobEnabled() {
         StoredDocument storedDocumentWithContent = StoredDocument.builder()
             .documentContentVersions(List.of(DocumentContentVersion.builder()
                 .build()))
@@ -233,7 +235,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testHardDeleteWithManyVersions() {
+    void testHardDeleteWithManyVersions() {
 
         StoredDocument storedDocumentWithContent = STORED_DOCUMENT;
         storedDocumentService.addStoredDocumentVersion(STORED_DOCUMENT, TEST_FILE);
@@ -247,7 +249,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testUpdateItems() {
+    void testUpdateItems() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocument.setId(UUID.randomUUID());
         storedDocument.setMetadata(Maps.newHashMap("Key", "Value"));
@@ -264,7 +266,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testUpdateDocument() {
+    void testUpdateDocument() {
         StoredDocument storedDocument = new StoredDocument();
         UpdateDocumentCommand command = new UpdateDocumentCommand();
         Date newTtl = new Date();
@@ -274,7 +276,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testUpdateDocumentWithMetaData() {
+    void testUpdateDocumentWithMetaData() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocument.setMetadata(Maps.newHashMap("Key", "Value"));
 
@@ -287,39 +289,50 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testUpdateDeletedDocument() {
+    void testUpdateDeletedDocument() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocument.setDeleted(true);
         UpdateDocumentCommand command = new UpdateDocumentCommand();
         Date newTtl = new Date();
         command.setTtl(newTtl);
         storedDocumentService.updateStoredDocument(storedDocument, command);
-        Assert.assertNull(storedDocument.getTtl());
+        assertNull(storedDocument.getTtl());
     }
 
     @Test
-    public void testFindAllExpiredStoredDocuments() {
+    void testFindAllExpiredStoredDocuments() {
         storedDocumentService.findAllExpiredStoredDocuments();
         verify(storedDocumentRepository, times(1)).findByTtlLessThanAndHardDeleted(any(), any());
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testUpdateStoredDocumentNullStoredDocument() {
-        storedDocumentService.updateStoredDocument(null, new UpdateDocumentCommand());
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testUpdateStoredDocumentNullCommand() {
-        storedDocumentService.updateStoredDocument(new StoredDocument(), null);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testUpdateMigratedStoredDocumentNullStoredDocument() {
-        storedDocumentService.updateMigratedStoredDocument(null, null);
+    @Test
+    void testUpdateStoredDocumentNullStoredDocument() {
+        var updateCommand = new UpdateDocumentCommand();
+        assertThrows(NullPointerException.class, () ->
+            storedDocumentService.updateStoredDocument(null, updateCommand)
+        );
     }
 
     @Test
-    public void testUpdateMigratedStoredDocumentIsDeleted() {
+    void testUpdateStoredDocumentNullCommand() {
+        var storedDoc = new StoredDocument();
+        assertThrows(NullPointerException.class, () ->
+            storedDocumentService.updateStoredDocument(storedDoc, null)
+        );
+    }
+
+
+    @Test
+    void testUpdateMigratedStoredDocumentNullStoredDocument() {
+        NullPointerException exception = assertThrows(
+            NullPointerException.class,
+            () -> storedDocumentService.updateMigratedStoredDocument(null, null)
+        );
+        assertEquals("Expected exception message", exception.getMessage());
+    }
+
+    @Test
+    void testUpdateMigratedStoredDocumentIsDeleted() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocument.setDeleted(true);
         storedDocumentService.updateMigratedStoredDocument(storedDocument, null);
@@ -327,7 +340,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testUpdateMigratedStoredDocumentNullMetadata() {
+    void testUpdateMigratedStoredDocumentNullMetadata() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocument.setDeleted(false);
         storedDocumentService.updateMigratedStoredDocument(storedDocument, null);
@@ -335,7 +348,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testUpdateItemsOverrideTrue() {
+    void testUpdateItemsOverrideTrue() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocument.setId(UUID.randomUUID());
         storedDocument.setMetadata(Maps.newHashMap("Key1", "Value1"));
@@ -356,7 +369,7 @@ public class StoredDocumentServiceTests {
     }
 
     @Test
-    public void testUpdateItemsOverrideFalse() {
+    void testUpdateItemsOverrideFalse() {
         StoredDocument storedDocument = new StoredDocument();
         storedDocument.setId(UUID.randomUUID());
         storedDocument.setMetadata(Maps.newHashMap("Key1", "Value1"));
