398aaf0b20cd971379b8e8585e687468d2e0a446
mdayican <mdayican@gmail.com>
2025-01-02 09:53:29 +0000

    fix more sonar issues-const

--- src/functionalTest/java/uk/gov/hmcts/dm/functional/UpdateDocumentIT.java
+++ src/functionalTest/java/uk/gov/hmcts/dm/functional/UpdateDocumentIT.java
@@ -9,23 +9,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class UpdateDocumentIT extends BaseIT {
+
+
+    private static final String METADATA_CONST = "metadata";
+    private static final String DOCUMENTS_CONST = "documents";
+    private static final String DOCUMENT_ID_CONST = "documentId";
+    private static final String DOCUMENTS_PATH = "/documents";
+    private static final String META_KEY2_CONST = "metakey2";
+    private static final String META_KEY2_VALUE = "metavalue2";
+
+    private static final String META_KEY_CONST = "metakey";
+    private static final String META_KEY_VALUE = "metavalue";
+
+    private static final String FUTURE_DATE_3000 = "3000-10-31T10:10:10+0000";
 
     @Test
     public void ud1UpdateTtlForADocument() {
 
         String documentUrl = createDocumentAndGetUrlAs(getCitizen());
 
-        LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
+        Map<String, String> map = Map.of("ttl", FUTURE_DATE_3000);
         givenRequest(getCitizen())
             .body(map)
             .contentType(ContentType.JSON)
             .expect()
             .statusCode(200)
-            .body("ttl", Matchers.equalTo("3000-10-31T10:10:10+0000"))
+            .body("ttl", Matchers.equalTo(FUTURE_DATE_3000))
             .when()
             .patch(documentUrl);
     }
@@ -35,8 +48,7 @@ public class UpdateDocumentIT extends BaseIT {
 
         String documentUrl = createDocumentAndGetUrlAs(getCitizen());
 
-        LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
+        Map<String, String> map = Map.of("ttl", FUTURE_DATE_3000);
         givenRequest(getCitizen2())
             .body(map)
             .contentType(ContentType.JSON)
@@ -51,8 +63,7 @@ public class UpdateDocumentIT extends BaseIT {
 
         String documentUrl = createDocumentAndGetUrlAs(getCitizen());
 
-        LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
+        Map<String, String> map = Map.of("ttl", FUTURE_DATE_3000);
         givenRequest(getCaseWorker())
             .body(map)
             .contentType(ContentType.JSON)
@@ -67,10 +78,8 @@ public class UpdateDocumentIT extends BaseIT {
 
         String documentUrl = createDocumentAndGetUrlAs(getCitizen());
 
-        LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
-        LinkedHashMap<String, String> map1 = new LinkedHashMap<>(1);
-        map1.put("ttl", "3000-01-31T10:10:10+0000");
+        Map<String, String> map = Map.of("ttl", FUTURE_DATE_3000);
+        Map<String, String> map1 = Map.of("ttl", FUTURE_DATE_3000);
         givenRequest(getCitizen())
             .body(map)
             .body(map1)
@@ -87,14 +96,13 @@ public class UpdateDocumentIT extends BaseIT {
 
         String documentUrl = createDocumentAndGetUrlAs(getCitizen());
 
-        LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
+        Map<String, String> map = Map.of("ttl", FUTURE_DATE_3000);
         givenRequest(getCitizen())
             .body(map)
             .contentType(ContentType.JSON)
             .expect()
             .statusCode(200)
-            .body("ttl", Matchers.equalTo("3000-10-31T10:10:10+0000"))
+            .body("ttl", Matchers.equalTo(FUTURE_DATE_3000))
             .when()
             .patch(documentUrl);
 
@@ -109,7 +117,7 @@ public class UpdateDocumentIT extends BaseIT {
         givenRequest(getCitizen())
             .expect()
             .statusCode(200)
-            .body("ttl", Matchers.equalTo("3000-10-31T10:10:10+0000"))
+            .body("ttl", Matchers.equalTo(FUTURE_DATE_3000))
             .when()
             .get(documentUrl);
     }
@@ -126,26 +134,24 @@ public class UpdateDocumentIT extends BaseIT {
         final String documentId2 = split[split.length - 1];
 
 
-        LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(2);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
-        LinkedHashMap<String, Object> map1 = new LinkedHashMap<>(2);
-        map1.put("id", documentId);
-        LinkedHashMap<String, String> map2 = new LinkedHashMap<>(1);
-        map2.put("metakey", "metavalue");
-        map1.put("metadata", map2);
-        LinkedHashMap<String, Object> map3 = new LinkedHashMap<>(2);
-        map3.put("id", documentId2);
-        LinkedHashMap<String, String> map4 = new LinkedHashMap<>(1);
-        map4.put("metakey2", "metavalue2");
-        map3.put("metadata", map4);
-        map.put("documents", new ArrayList<>(Arrays.asList(map1, map3)));
+
+        Map<String, String> map2 = Map.of(META_KEY_CONST, META_KEY_VALUE);
+        Map<String, Object> map1 = Map.of("id", documentId, METADATA_CONST, map2);
+
+        Map<String, String> map4 = Map.of(META_KEY2_CONST, META_KEY2_VALUE);
+        Map<String, Object> map3 = Map.of("id", documentId2, METADATA_CONST, map4);
+
+        Map<String, Serializable> map =
+            Map.of("ttl", FUTURE_DATE_3000,
+                DOCUMENTS_CONST, new ArrayList<>(Arrays.asList(map1, map3)));
+
         givenRequest(getCitizen())
             .body(map)
             .contentType(ContentType.JSON)
             .expect().log().all()
             .statusCode(500)
             .when()
-            .patch("/documents");
+            .patch(DOCUMENTS_PATH);
     }
 
     @Test
@@ -159,19 +165,14 @@ public class UpdateDocumentIT extends BaseIT {
             .split("/");
         final String documentId2 = split[split.length - 1];
 
-        LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(2);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
-        LinkedHashMap<String, Object> map1 = new LinkedHashMap<>(2);
-        map1.put("documentId", documentId);
-        LinkedHashMap<String, String> map2 = new LinkedHashMap<>(1);
-        map2.put("metakey", "metavalue");
-        map1.put("metadata", map2);
-        LinkedHashMap<String, Object> map3 = new LinkedHashMap<>(2);
-        map3.put("documentId", documentId2);
-        LinkedHashMap<String, String> map4 = new LinkedHashMap<>(1);
-        map4.put("metakey2", "metavalue2");
-        map3.put("metadata", map4);
-        map.put("documents", new ArrayList<>(Arrays.asList(map1, map3)));
+        Map<String, String> map2 = Map.of(META_KEY_CONST, META_KEY_VALUE);
+        Map<String, Object> map1 = Map.of(DOCUMENT_ID_CONST, documentId, METADATA_CONST, map2);
+
+        Map<String, String> map4 = Map.of(META_KEY2_CONST, META_KEY2_VALUE);
+        Map<String, Object> map3 = Map.of(DOCUMENT_ID_CONST, documentId2, METADATA_CONST, map4);
+
+        Map<String, Serializable> map
+            = Map.of("ttl", FUTURE_DATE_3000, DOCUMENTS_CONST, new ArrayList<>(Arrays.asList(map1, map3)));
         givenRequest(getCitizen())
             .body(map)
             .contentType(ContentType.JSON)
@@ -179,7 +180,7 @@ public class UpdateDocumentIT extends BaseIT {
             .statusCode(200)
             .body("result", Matchers.equalTo("Success"))
             .when()
-            .patch("/documents");
+            .patch(DOCUMENTS_PATH);
     }
 
     @Test
@@ -191,18 +192,18 @@ public class UpdateDocumentIT extends BaseIT {
         final UUID uuid = UUID.randomUUID();
 
         LinkedHashMap<String, Serializable> map = new LinkedHashMap<>(2);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
+        map.put("ttl", FUTURE_DATE_3000);
         LinkedHashMap<String, Serializable> map1 = new LinkedHashMap<>(2);
-        map1.put("documentId", documentId);
+        map1.put(DOCUMENT_ID_CONST, documentId);
         LinkedHashMap<String, String> map2 = new LinkedHashMap<>(1);
-        map2.put("metakey", "metavalue");
-        map1.put("metadata", map2);
+        map2.put(META_KEY_CONST, META_KEY_VALUE);
+        map1.put(METADATA_CONST, map2);
         LinkedHashMap<String, Serializable> map3 = new LinkedHashMap<>(2);
-        map3.put("documentId", uuid);
+        map3.put(DOCUMENT_ID_CONST, uuid);
         LinkedHashMap<String, String> map4 = new LinkedHashMap<>(1);
-        map4.put("metakey2", "metavalue2");
-        map3.put("metadata", map4);
-        map.put("documents", new ArrayList<>(List.of(map1, map3)));
+        map4.put(META_KEY2_CONST, META_KEY2_VALUE);
+        map3.put(METADATA_CONST, map4);
+        map.put(DOCUMENTS_CONST, new ArrayList<>(List.of(map1, map3)));
         givenRequest(getCitizen())
             .body(map)
             .contentType(ContentType.JSON)
@@ -210,7 +211,7 @@ public class UpdateDocumentIT extends BaseIT {
             .statusCode(404)
             .body("error", Matchers.equalTo("Document with ID: " + uuid + " could not be found"))
             .when()
-            .patch("/documents");
+            .patch(DOCUMENTS_PATH);
     }
 
     @Test
@@ -223,8 +224,7 @@ public class UpdateDocumentIT extends BaseIT {
         String nonExistentId = UUID.randomUUID().toString();
         documentUrl = documentUrl.replace(documentId, nonExistentId);
 
-        LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
-        map.put("ttl", "3000-10-31T10:10:10+0000");
+        Map<String, String> map = Map.of("ttl", FUTURE_DATE_3000);
         givenRequest(getCitizen())
             .body(map)
             .contentType(ContentType.JSON)
