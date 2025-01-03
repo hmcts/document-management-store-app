6c3f35cb72a0086af86b4f9d95bb806a6b595e61
mdayican <mdayican@gmail.com>
2025-01-03 11:09:03 +0000

    fix sonar for ErrorPageIT

--- src/functionalTest/java/uk/gov/hmcts/dm/functional/ErrorPageIT.java
+++ src/functionalTest/java/uk/gov/hmcts/dm/functional/ErrorPageIT.java
@@ -8,8 +8,22 @@ import org.springframework.http.MediaType;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.util.MimeTypeUtils.ALL_VALUE;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_XML_VALUE;
 
 public class ErrorPageIT extends BaseIT {
+
+    private static final String DOCUMENTS_CONST = "documents";
+    private static final String FILES_CONST = "files";
+    private static final String ROLES_CONST = "roles";
+    private static final String CITIZEN_CONST = "citizen";
+    private static final String CLASSIFICATION_CONST = "classification";
+    private static final String ERROR_CONST = "error";
+    private static final String DOCTYPE_HTML_CONST = "<!DOCTYPE html>";
+    private static final String MIME_TYPE_GOV_APP =
+        "application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8";
+    private static final String ERROR_MESSAGE = "Your upload contains a disallowed file type";
+    private static final String DOCUMENTS_PATH = "/documents";
 
     @Test
     public void ep1AsAnUnauthenticatedWebUserTryingToAccessADocumentReceiveJsonErrorPageWith403() {
@@ -28,7 +42,7 @@ public class ErrorPageIT extends BaseIT {
     public void ep2AsAnAuthenticatedUserTryingToAccessAnUnknownDocumentReceiveJsonErrorPageWith404() {
 
         givenRequest(getCitizen())
-            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .accept(MIME_TYPE_GOV_APP)
             .expect()
             .statusCode(404)
             .contentType(ContentType.JSON)
@@ -40,24 +54,24 @@ public class ErrorPageIT extends BaseIT {
     public void ep3AsAnAuthenticatedUserTryingToAccessDocumentDirReceiveJsonErrorPageWith405() {
 
         givenRequest(getCitizen())
-            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .accept(MIME_TYPE_GOV_APP)
             .expect()
             .statusCode(405)
             .contentType(ContentType.JSON)
             .when()
-            .get("documents");
+            .get(DOCUMENTS_CONST);
     }
 
     @Test
     public void ep4AsAnAuthenticatedUserTryingToPostNoDocumentReceiveJsonErrorPageWith415() {
 
         givenRequest(getCitizen())
-            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .accept(MIME_TYPE_GOV_APP)
             .expect()
             .statusCode(415)
             .contentType(ContentType.JSON)
             .when()
-            .post("documents");
+            .post(DOCUMENTS_CONST);
     }
 
     @Test
@@ -65,7 +79,7 @@ public class ErrorPageIT extends BaseIT {
     public void ep5AsAnAuthenticatedUserTryingToPostBadAttachmentReceiveJsonErrorPageWith415() {
 
         givenRequest(getCitizen())
-            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .accept(MIME_TYPE_GOV_APP)
             .multiPart("file", file(getBadAttachment1()), MediaType.ALL_VALUE)
             .expect()
             .statusCode(422)
@@ -80,7 +94,7 @@ public class ErrorPageIT extends BaseIT {
         String url = createDocumentAndGetUrlAs(getCitizen());
 
         givenRequest(getCitizen2())
-            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json,application/json;charset=UTF-8")
+            .accept(MIME_TYPE_GOV_APP)
             .multiPart("file", file(getAttachment9Jpg()), MediaType.IMAGE_JPEG_VALUE)
             .expect()
             .statusCode(403)
@@ -99,12 +113,13 @@ public class ErrorPageIT extends BaseIT {
             .expect()
             .statusCode(403)
             .contentType(ContentType.JSON)
-            .body(not(containsString("<!DOCTYPE html>")))
+            .body(not(containsString(DOCTYPE_HTML_CONST)))
             .when()
             .get(documentUrl);
     }
 
     @Test
+    // ep9AsAnUnauthenticatedApiUserTryingToAccessADocumentWithDocumentAcceptHeaderReceiveJsonError
     public void ep8AsAnUnauthenticatedApiUserTryingToAccessADocumentWithNoAcceptHeaderReceiveJsonError() {
 
         String documentUrl = createDocumentAndGetUrlAs(getCitizen());
@@ -113,21 +128,7 @@ public class ErrorPageIT extends BaseIT {
             .expect()
             .statusCode(403)
             .contentType(ContentType.JSON)
-            .body(not(containsString("<!DOCTYPE html>")))
-            .when()
-            .get(documentUrl);
-    }
-
-    @Test
-    public void ep9AsAnUnauthenticatedApiUserTryingToAccessADocumentWithDocumentAcceptHeaderReceiveJsonError() {
-
-        String documentUrl = createDocumentAndGetUrlAs(getCitizen());
-
-        givenUnauthenticatedRequest()
-            .expect()
-            .statusCode(403)
-            .contentType(ContentType.JSON)
-            .body(not(containsString("<!DOCTYPE html>")))
+            .body(not(containsString(DOCTYPE_HTML_CONST)))
             .when()
             .get(documentUrl);
     }
@@ -155,39 +156,39 @@ public class ErrorPageIT extends BaseIT {
             .multiPart("file", file(getAttachment18()), MediaType.APPLICATION_XML_VALUE)
             .expect()
             .statusCode(422)
-            .body(not(containsString("<!DOCTYPE html>")))
+            .body(not(containsString(DOCTYPE_HTML_CONST)))
             .when()
-            .post("documents");
+            .post(DOCUMENTS_CONST);
     }
 
     @Test
     public void ep12AsAnAuthenticatedUserWhenIPostASvgDocumentIShouldGetJsonResponse() {
 
         givenRequest(getCitizen())
-            .multiPart("files", file(getAttachment10()), V1MimeTypes.IMAGE_SVG_VALUE)
-            .multiPart("classification", String.valueOf(Classifications.PUBLIC))
-            .multiPart("roles", "citizen")
+            .multiPart(FILES_CONST, file(getAttachment10()), V1MimeTypes.IMAGE_SVG_VALUE)
+            .multiPart(CLASSIFICATION_CONST, String.valueOf(Classifications.PUBLIC))
+            .multiPart(ROLES_CONST, CITIZEN_CONST)
             .expect().log().all()
             .statusCode(422)
-            .body("error", equalTo("Your upload contains a disallowed file type"))
+            .body(ERROR_CONST, equalTo(ERROR_MESSAGE))
             .contentType(ContentType.JSON)
             .when()
-            .post("/documents");
+            .post(DOCUMENTS_PATH);
     }
 
     @Test
     public void ep13AsAnAuthenticatedUserWhenIPostAXmlDocumentIShouldGetJsonResponse() {
 
         givenRequest(getCitizen())
-            .multiPart("files", file(getAttachment18()), V1MimeTypes.APPLICATION_XML_VALUE)
-            .multiPart("classification", String.valueOf(Classifications.PUBLIC))
-            .multiPart("roles", "citizen")
+            .multiPart(FILES_CONST, file(getAttachment18()), APPLICATION_XML_VALUE)
+            .multiPart(CLASSIFICATION_CONST, String.valueOf(Classifications.PUBLIC))
+            .multiPart(ROLES_CONST, CITIZEN_CONST)
             .expect().log().all()
             .statusCode(422)
-            .body("error", equalTo("Your upload contains a disallowed file type"))
+            .body(ERROR_CONST, equalTo(ERROR_MESSAGE))
             .contentType(ContentType.JSON)
             .when()
-            .post("/documents");
+            .post(DOCUMENTS_PATH);
     }
 
     @Test
@@ -195,14 +196,14 @@ public class ErrorPageIT extends BaseIT {
     public void ep14AsAnAuthenticatedUserWhenIPostAExeDocumentIShouldGetJsonResponse() {
 
         givenRequest(getCitizen())
-            .multiPart("files", file(getBadAttachment1()), V1MimeTypes.ALL_VALUE)
-            .multiPart("classification", String.valueOf(Classifications.PUBLIC))
-            .multiPart("roles", "citizen")
+            .multiPart(FILES_CONST, file(getBadAttachment1()), ALL_VALUE)
+            .multiPart(CLASSIFICATION_CONST, String.valueOf(Classifications.PUBLIC))
+            .multiPart(ROLES_CONST, CITIZEN_CONST)
             .expect().log().all()
             .statusCode(422)
-            .body("error", equalTo("Your upload contains a disallowed file type"))
+            .body(ERROR_CONST, equalTo(ERROR_MESSAGE))
             .contentType(ContentType.JSON)
             .when()
-            .post("/documents");
+            .post(DOCUMENTS_PATH);
     }
 }
