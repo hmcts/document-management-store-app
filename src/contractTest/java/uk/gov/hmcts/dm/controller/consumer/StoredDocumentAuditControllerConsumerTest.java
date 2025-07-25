3ed7089e993cf43fefa39ab59434aa353e08db7d
mdayican <mdayican@gmail.com>
2025-07-25 14:40:11 +0100

    remove recordedDateTime

--- src/contractTest/java/uk/gov/hmcts/dm/controller/consumer/StoredDocumentAuditControllerConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/consumer/StoredDocumentAuditControllerConsumerTest.java
@@ -74,7 +74,6 @@ public class StoredDocumentAuditControllerConsumerTest extends BaseConsumerPactT
             .stringType("action", "READ")
             .stringType("username", "user@example.com")
             .stringType("type", "StoredDocumentAuditEntry")
-            .stringType("recordedDateTime")
             .object("_links", links -> {
                 links.object("document", docLink ->
                     docLink.stringType("href", "http://localhost/documents/" + DOCUMENT_ID)
