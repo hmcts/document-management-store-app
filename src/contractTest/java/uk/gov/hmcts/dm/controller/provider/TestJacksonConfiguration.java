523762f6870b8bf15120051a2b0bc40bb196cc6b
mdayican <mdayican@gmail.com>
2025-07-25 09:26:24 +0100

    add more testconfig

--- src/contractTest/java/uk/gov/hmcts/dm/controller/provider/TestJacksonConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/provider/TestJacksonConfiguration.java
@@ -1,8 +1,9 @@
 package uk.gov.hmcts.dm.controller.provider;
 
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.mediatype.hal.HalConfiguration;
 import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
 
@@ -10,12 +11,30 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
 public class TestJacksonConfiguration {
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer halModuleCustomizer() {
-        return builder -> builder.modulesToInstall(new Jackson2HalModule());
+    public WebMvcLinkBuilderFactory linkBuilderFactory() {
+        return new WebMvcLinkBuilderFactory();
+    }
+
+
+    @Bean
+    public Jackson2HalModule halModule() {
+        return new Jackson2HalModule();
     }
 
     @Bean
-    public WebMvcLinkBuilderFactory linkBuilderFactory() {
+    public HalConfiguration halConfiguration() {
+        return new HalConfiguration();
+    }
+
+    @Bean
+    public WebMvcLinkBuilderFactory webMvcLinkBuilderFactory() {
         return new WebMvcLinkBuilderFactory();
+    }
+
+    @Bean
+    public ObjectMapper halObjectMapper(Jackson2HalModule halModule) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(halModule);
+        return objectMapper;
     }
 }
