23cdb10f1a435441aef6c726ca5e03ea6daae90e
mdayican <mdayican@gmail.com>
2025-07-25 08:30:18 +0100

    fix testconfig

--- src/contractTest/java/uk/gov/hmcts/dm/controller/provider/TestJacksonConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/provider/TestJacksonConfiguration.java
@@ -1,20 +1,14 @@
 package uk.gov.hmcts.dm.controller.provider;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
 
 @TestConfiguration
 public class TestJacksonConfiguration {
-    @Autowired
-    void configureObjectMapper(ObjectMapper objectMapper) {
-        objectMapper.registerModule(new Jackson2HalModule());
-    }
-
     @Bean
-    public Jackson2HalModule halModule() {
-        return new Jackson2HalModule();
+    public Jackson2ObjectMapperBuilderCustomizer halModuleCustomizer() {
+        return builder -> builder.modulesToInstall(new Jackson2HalModule());
     }
 }
