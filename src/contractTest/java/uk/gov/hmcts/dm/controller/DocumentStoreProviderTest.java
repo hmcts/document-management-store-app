2946017c2565389854f1401371f6491fc4dd506e
mdayican <mdayican@gmail.com>
2025-07-29 13:56:50 +0100

    EM-6470 pact for audit endpoint (#2789)

--- src/contractTest/java/uk/gov/hmcts/dm/controller/DocumentStoreProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/DocumentStoreProviderTest.java
@@ -10,18 +10,11 @@ import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.dm.controller.provider.BaseProviderTest;
 import uk.gov.hmcts.dm.domain.DocumentContentVersion;
 import uk.gov.hmcts.dm.domain.StoredDocument;
-import uk.gov.hmcts.dm.service.AuditedStoredDocumentOperationsService;
-import uk.gov.hmcts.dm.service.DocumentContentVersionService;
-import uk.gov.hmcts.dm.service.SearchService;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
@@ -30,21 +23,8 @@ import java.util.UUID;
 import static org.mockito.Mockito.when;
 
 @Provider("em_dm_store")
-@WebMvcTest({StoredDocumentController.class, StoredDocumentDeleteController.class})
 @Import(StoreDocumentControllerTestConfiguration.class)
 public class DocumentStoreProviderTest extends BaseProviderTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockitoBean
-    private AuditedStoredDocumentOperationsService auditedStoredDocumentOperationsService;
-
-    @MockitoBean
-    protected DocumentContentVersionService documentContentVersionService;
-
-    @MockitoBean
-    private SearchService searchService;
 
     private final UUID id = UUID.fromString("5c3c3906-2b51-468e-8cbb-a4002eded075");
 
