b2e07943396ecaa0ee3a7f4105585525e366340e
mdayican <mdayican@gmail.com>
2025-07-29 06:54:48 +0100

    fix mocking

--- src/contractTest/java/uk/gov/hmcts/dm/controller/provider/StoredDocumentUpdateControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/provider/StoredDocumentUpdateControllerProviderTest.java
@@ -2,13 +2,14 @@ package uk.gov.hmcts.dm.controller.provider;
 
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
+import uk.gov.hmcts.dm.domain.StoredDocument;
 
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @Provider("dm_store_update_document_provider")
 public class StoredDocumentUpdateControllerProviderTest extends BaseProviderTest {
@@ -16,10 +17,10 @@ public class StoredDocumentUpdateControllerProviderTest extends BaseProviderTest
 
     @State("Documents exist and can be updated with new TTL and metadata")
     public void documentsExistToUpdate() {
-        doNothing().when(auditedStoredDocumentOperationsService).updateDocument(
+        when(auditedStoredDocumentOperationsService.updateDocument(
             any(UUID.class),
             any(Map.class),
-            any(Date.class)
-        );
+            any(Date.class))
+        ).thenReturn(new StoredDocument());
     }
 }
