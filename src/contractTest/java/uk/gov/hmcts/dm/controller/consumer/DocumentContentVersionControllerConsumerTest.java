cf129af02955da5d3c54aad774eae6edbf414ae8
mdayican <mdayican@gmail.com>
2025-08-05 17:02:19 +0100

    EM-6466 add pact test for POST version endpoint (#2804)
    * EM-6466 add pact for post version
    
    * fix request
    
    * fix multipart file
    
    * fix multipart file
    
    * pass file content
    
    * fix consumer
    
    * fix ids
    
    * remove unnecessary fields
    
    * fix urls and location header
    
    ---------
    
    Co-authored-by: yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>

---
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/consumer/DocumentContentVersionControllerConsumerTest.java
@@ -0,0 +1,116 @@
+package uk.gov.hmcts.dm.controller.consumer;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import static au.com.dius.pact.consumer.dsl.LambdaDsl.newJsonBody;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+public class DocumentContentVersionControllerConsumerTest extends BaseConsumerPactTest {
+
+    private static final String PROVIDER = "dm_store_document_content_version_provider";
+    private static final String CONSUMER = "dm_store_document_content_version_consumer";
+    private static final String DOCUMENT_ID = "969983aa-52ae-41bd-8cf3-4aabcc120783";
+    private static final String DOCUMENT_CONTENT_VERSION_ID = "2216a872-81f7-4cad-a474-32a59608b038";
+    private static final String PATH = "/documents/" + DOCUMENT_ID + "/versions";
+
+    private static final byte[] FILE_BYTES;
+
+    static {
+        try {
+            FILE_BYTES = Files.readAllBytes(
+                Paths.get(ClassLoader.getSystemResource("test-files/sample.pdf").toURI())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Pact(provider = PROVIDER, consumer = CONSUMER)
+    public V4Pact addDocumentContentVersionPact(PactDslWithProvider builder) throws IOException {
+        return builder
+            .given("Can add Document Content Version and associate it with a given Stored Document.")
+            .uponReceiving("POST multipart request to upload a document version")
+            .method("POST")
+            .withFileUpload(
+                "file",
+                "sample.pdf",
+                "application/pdf",
+                FILE_BYTES
+            )
+            .path(PATH)
+            .headers(Map.of(
+                "ServiceAuthorization", "Bearer some-s2s-token",
+                "Accept", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8"
+            ))
+            .willRespondWith()
+            .status(201)
+            .headers(Map.of(
+                "Content-Type", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8",
+                "Location", "http://localhost/documents/" + DOCUMENT_ID + "/versions/" + DOCUMENT_CONTENT_VERSION_ID
+            ))
+            .body(buildResponseDsl())
+            .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "addDocumentContentVersionPact")
+    void testAddDocumentVersion(MockServer mockServer) throws URISyntaxException, IOException {
+        given()
+            .baseUri(mockServer.getUrl())
+            .contentType(ContentType.MULTIPART)
+            .multiPart("file", "sample.pdf", FILE_BYTES, "application/pdf")
+            .headers(Map.of(
+                "ServiceAuthorization", "Bearer some-s2s-token",
+                "Accept", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8"
+            ))
+            .when()
+            .post(PATH)
+            .then()
+            .log().all()
+            .statusCode(201)
+            .body("mimeType", equalTo("application/pdf"))
+            .body("originalDocumentName", equalTo("sample.pdf"))
+            .body("_links.self.href", containsString("/documents/" + DOCUMENT_ID + "/versions/"))
+            .body("_links.binary.href", containsString("/binary"))
+            .body("_links.document.href", containsString("/documents/" + DOCUMENT_ID));
+    }
+
+    private DslPart buildResponseDsl() {
+        return newJsonBody((body) -> {
+            body
+                .stringType("mimeType", "application/pdf")
+                .stringType("originalDocumentName", "sample.pdf")
+                .stringType("createdBy", "test-user")
+                .numberType("size", 1024)
+                .object("_links", links -> {
+                    links
+                        .object("self", self -> self.stringType("href",
+                            "http://localhost/documents/" + DOCUMENT_ID
+                                + "/versions/" + DOCUMENT_CONTENT_VERSION_ID))
+                        .object("binary", binary -> binary.stringType("href",
+                            "http://localhost/documents/" + DOCUMENT_ID
+                                + "/versions/" + DOCUMENT_CONTENT_VERSION_ID + "/binary"))
+                        .object("document",
+                            document ->
+                                document.stringType("href", "http://localhost/documents/" + DOCUMENT_ID));
+                });
+        }).build();
+    }
+}
