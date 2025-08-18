e26f17718892c98293f047497269181471df64f1
mdayican <mdayican@gmail.com>
2025-08-18 12:45:49 +0100

    EM-6465 add pact test

--- src/contractTest/java/uk/gov/hmcts/dm/controller/consumer/DocumentContentVersionControllerConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/consumer/DocumentContentVersionControllerConsumerTest.java
@@ -26,7 +26,8 @@ public class DocumentContentVersionControllerConsumerTest extends BaseConsumerPa
     private static final String CONSUMER = "dm_store_document_content_version_consumer";
     private static final String DOCUMENT_ID = "969983aa-52ae-41bd-8cf3-4aabcc120783";
     private static final String DOCUMENT_CONTENT_VERSION_ID = "2216a872-81f7-4cad-a474-32a59608b038";
-    private static final String PATH = "/documents/" + DOCUMENT_ID + "/versions";
+    private static final String PATH_VERSIONS = "/documents/" + DOCUMENT_ID + "/versions";
+    private static final String PATH_LEGACY_ENDPOINT = "/documents/" + DOCUMENT_ID;
 
     private static final byte[] FILE_BYTES;
 
@@ -54,7 +55,7 @@ public class DocumentContentVersionControllerConsumerTest extends BaseConsumerPa
                 "application/pdf",
                 FILE_BYTES
             )
-            .path(PATH)
+            .path(PATH_VERSIONS)
             .headers(Map.of(
                 "ServiceAuthorization", "Bearer some-s2s-token",
                 "Accept", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8"
@@ -81,7 +82,7 @@ public class DocumentContentVersionControllerConsumerTest extends BaseConsumerPa
                 "Accept", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8"
             ))
             .when()
-            .post(PATH)
+            .post(PATH_VERSIONS)
             .then()
             .log().all()
             .statusCode(201)
@@ -91,6 +92,59 @@ public class DocumentContentVersionControllerConsumerTest extends BaseConsumerPa
             .body("_links.binary.href", containsString("/binary"))
             .body("_links.document.href", containsString("/documents/" + DOCUMENT_ID));
     }
+
+
+
+    @Pact(provider = PROVIDER, consumer = CONSUMER)
+    public V4Pact addDocumentContentVersionLegacyMappingPact(PactDslWithProvider builder) throws IOException {
+        return builder
+            .given("Can add Document Content Version and associate it with a given Stored Document.")
+            .uponReceiving("POST multipart request to upload a document version using legacy endpoint")
+            .method("POST")
+            .withFileUpload(
+                "file",
+                "sample.pdf",
+                "application/pdf",
+                FILE_BYTES
+            )
+            .path(PATH_LEGACY_ENDPOINT)
+            .headers(Map.of(
+                "ServiceAuthorization", "Bearer some-s2s-token",
+                "Accept", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8"
+            ))
+            .willRespondWith()
+            .status(201)
+            .headers(Map.of(
+                "Content-Type", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8",
+                "Location", "http://localhost/documents/" + DOCUMENT_ID + "/versions/" + DOCUMENT_CONTENT_VERSION_ID
+            ))
+            .body(buildResponseDsl())
+            .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "addDocumentContentVersionLegacyMappingPact")
+    void testAddDocumentVersionLegacy(MockServer mockServer) throws URISyntaxException, IOException {
+        given()
+            .baseUri(mockServer.getUrl())
+            .contentType(ContentType.MULTIPART)
+            .multiPart("file", "sample.pdf", FILE_BYTES, "application/pdf")
+            .headers(Map.of(
+                "ServiceAuthorization", "Bearer some-s2s-token",
+                "Accept", "application/vnd.uk.gov.hmcts.dm.documentContentVersion.v1+hal+json;charset=UTF-8"
+            ))
+            .when()
+            .post(PATH_LEGACY_ENDPOINT)
+            .then()
+            .log().all()
+            .statusCode(201)
+            .body("mimeType", equalTo("application/pdf"))
+            .body("originalDocumentName", equalTo("sample.pdf"))
+            .body("_links.self.href", containsString("/documents/" + DOCUMENT_ID + "/versions/"))
+            .body("_links.binary.href", containsString("/binary"))
+            .body("_links.document.href", containsString("/documents/" + DOCUMENT_ID));
+    }
+
 
     private DslPart buildResponseDsl() {
         return newJsonBody((body) -> {
