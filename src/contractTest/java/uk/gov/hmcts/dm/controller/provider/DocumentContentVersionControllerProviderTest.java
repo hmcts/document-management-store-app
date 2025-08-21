11318ce0dd3b9557691c2fcf3ce541e1d42a8670
mdayican <mdayican@gmail.com>
2025-08-21 17:45:49 +0100

    EM-6469 get endpoint

--- src/contractTest/java/uk/gov/hmcts/dm/controller/provider/DocumentContentVersionControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/provider/DocumentContentVersionControllerProviderTest.java
@@ -2,17 +2,22 @@ package uk.gov.hmcts.dm.controller.provider;
 
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.dm.domain.DocumentContentVersion;
 import uk.gov.hmcts.dm.domain.StoredDocument;
 import uk.gov.hmcts.dm.security.Classifications;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -84,5 +89,49 @@ public class DocumentContentVersionControllerProviderTest extends BaseProviderTe
         when(auditedDocumentContentVersionOperationsService.readDocumentContentVersion(DOCUMENT_CONTENT_VERSION_ID))
             .thenReturn(documentContentVersion);
     }
+
+    @State("A specific Document Content Version binary exists for a given Stored Document.")
+    public void documentContentVersionBinaryExists() throws IOException {
+        StoredDocument storedDocument = new StoredDocument();
+        storedDocument.setId(UUID.fromString(DOCUMENT_ID));
+        storedDocument.setClassification(Classifications.PUBLIC);
+        storedDocument.setCreatedBy("test-user");
+        storedDocument.setCreatedOn(new Date());
+        storedDocument.setRoles(Set.of("citizen"));
+
+        DocumentContentVersion documentContentVersion = new DocumentContentVersion(
+            DOCUMENT_CONTENT_VERSION_ID,
+            "application/pdf",
+            "sample.pdf",
+            "test-user",
+            "test-service",
+            new Date(),
+            storedDocument,
+            8L, // size matches dummyData length
+            "http://localhost/documents/" + DOCUMENT_ID + "/versions/" + DOCUMENT_CONTENT_VERSION_ID,
+            "abc123checksum"
+        );
+
+        when(documentContentVersionService.findById(DOCUMENT_CONTENT_VERSION_ID))
+            .thenReturn(Optional.of(documentContentVersion));
+
+
+        byte[] dummyData = new byte[]{
+            (byte) 0xFF, (byte) 0xD8, (byte) 0xFF, 0x00, 0x10, 0x20, 0x30, 0x40
+        };
+
+        // Mock binary streaming
+        doAnswer(invocation -> {
+            HttpServletResponse response = invocation.getArgument(2);
+            try (ServletOutputStream os = response.getOutputStream()) {
+                os.write(dummyData);
+                os.flush();
+            }
+            return null;
+        }).when(auditedDocumentContentVersionOperationsService)
+            .readDocumentContentVersionBinaryFromBlobStore(eq(documentContentVersion), any(), any());
+    }
+
+
 }
 
