aa08e604b1437d85e79f4b1a3c9aaa57e2529562
mdayican <mdayican@gmail.com>
2025-09-09 18:13:20 +0100

    Em-6471

---
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/consumer/StoredDocumentControllerMultipartConsumerTest.java
@@ -0,0 +1,101 @@
+package uk.gov.hmcts.dm.controller.consumer;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.MultipartBuilder;
+import au.com.dius.pact.consumer.dsl.PactBuilder;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import org.junit.jupiter.api.Test;
+
+import static au.com.dius.pact.consumer.dsl.LambdaDsl.newJsonBody;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class StoredDocumentControllerMultipartConsumerTest extends BaseConsumerPactTest {
+
+    private static final String PROVIDER = "dm_store_stored_document_multipart_provider";
+    private static final String CONSUMER = "dm_store_stored_document_multipart_consumer";
+
+    @Pact(provider = PROVIDER, consumer = CONSUMER)
+    public V4Pact uploadDocumentsPact(PactBuilder builder) {
+        return builder
+            .given("Can create Stored Documents from multipart upload")
+
+            .expectsToReceiveHttpInteraction("POST request to upload documents", http -> http
+                .withRequest(request -> request
+                    .path("/documents")
+                    .method("POST")
+                    .header("ServiceAuthorization", "Bearer some-s2s-token")
+                    .header("Accept", "application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json;charset=UTF-8")
+                    .body(new MultipartBuilder()
+                        .binaryPart("files", "test-file.txt", "Hello World".getBytes(), "text/plain")
+                        .textPart("classification", "PUBLIC", "text/plain")
+                        .textPart("roles", "citizen", "text/plain")
+                    )
+                )
+                .willRespondWith(response -> response
+                    .status(200)
+                    .header("Content-Type",
+                        "application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json;charset=UTF-8")
+                    .body(buildUploadResponseDsl())
+                )
+            )
+            .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "uploadDocumentsPact", providerName = PROVIDER)
+    void testUploadDocuments(MockServer mockServer) {
+        given()
+            .baseUri(mockServer.getUrl())
+            .accept("application/vnd.uk.gov.hmcts.dm.document-collection.v1+hal+json;charset=UTF-8")
+            .header("ServiceAuthorization", "Bearer some-s2s-token")
+            // Must match DTO: "files"
+            .multiPart("files", "test-file.txt", "Hello World".getBytes(), "text/plain")
+            // Must match DTO: "classification" and be one of PUBLIC/PRIVATE/RESTRICTED
+            .multiPart("classification", "PUBLIC")
+            // Must match DTO: "roles"
+            .multiPart("roles", "citizen")
+            .when()
+            .post("/documents")
+            .then()
+            .log().all()
+            .statusCode(200)
+            .body("_embedded.storedDocumentHalResources[0].classification", equalTo("PUBLIC"))
+            .body("_embedded.storedDocumentHalResources[0].createdBy", equalTo("test-user-1"))
+            .body("_embedded.storedDocumentHalResources[1].classification", equalTo("PUBLIC"))
+            .body("_embedded.storedDocumentHalResources[1].createdBy", equalTo("test-user-2"));
+    }
+
+
+    private DslPart buildUploadResponseDsl() {
+        return newJsonBody(root -> {
+            root.object("_embedded", embedded -> {
+                embedded.array("storedDocumentHalResources", docs -> {
+                    // Document 1
+                    docs.object(doc -> {
+                        doc.stringType("classification", "PUBLIC");
+                        doc.stringType("createdBy", "test-user-1");
+                        doc.stringMatcher("createdOn", "\\d{4}-\\d{2}-\\d{2}T.*Z", "2024-01-01T12:00:00Z");
+                        doc.object("_links", links -> {
+                            links.object("self", self ->
+                                self.stringType("href", "http://localhost/documents/11111111-1111-1111-1111-111111111111"));
+                        });
+                    });
+                    // Document 2
+                    docs.object(doc -> {
+                        doc.stringType("classification", "PUBLIC");
+                        doc.stringType("createdBy", "test-user-2");
+                        doc.stringMatcher("createdOn", "\\d{4}-\\d{2}-\\d{2}T.*Z", "2024-01-01T12:00:00Z");
+                        doc.object("_links", links -> {
+                            links.object("self", self ->
+                                self.stringType("href", "http://localhost/documents/22222222-2222-2222-2222-222222222222"));
+                        });
+                    });
+                });
+            });
+        }).build();
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/provider/StoredDocumentMultipartProviderTest.java
@@ -0,0 +1,99 @@
+package uk.gov.hmcts.dm.controller.provider;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactBrokerConsumerVersionSelectors;
+import au.com.dius.pact.provider.junitsupport.loader.SelectorBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.dm.domain.StoredDocument;
+import uk.gov.hmcts.dm.security.Classifications;
+import uk.gov.hmcts.dm.service.AuditedStoredDocumentOperationsService;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator; // Import the validator
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@Provider("dm_store_stored_document_multipart_provider")
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles({"embedded", "contract", "componenttest"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PactBroker(
+    url = "${PACT_BROKER_FULL_URL:http://localhost:80}",
+    providerBranch = "${pact.provider.branch}"
+)
+public class StoredDocumentMultipartProviderTest {
+
+    @LocalServerPort
+    private int port;
+
+    // This is the key dependency used by your DmServiceAuthFilter
+    @MockitoBean
+    private AuthTokenValidator authTokenValidator;
+
+    // We still need to mock the service used by the controller
+    @MockitoBean
+    private AuditedStoredDocumentOperationsService auditedStoredDocumentOperationsService;
+
+
+    @PactBrokerConsumerVersionSelectors
+    public static SelectorBuilder consumerVersionSelectors() {
+        // ... (your existing selector logic)
+        return new SelectorBuilder().mainBranch();
+    }
+
+    @BeforeEach
+    void setupPactVerification(PactVerificationContext context) {
+        System.getProperties().setProperty("pact.verifier.publishResults", "true");
+        if (context != null) {
+            context.setTarget(new HttpTestTarget("localhost", port));
+        }
+    }
+
+
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        if (context != null) {
+            context.verifyInteraction();
+        }
+    }
+
+    @State("Can create Stored Documents from multipart upload")
+    public void canCreateStoredDocumentsFromMultipartUpload() {
+        // 1. Mock the AuthTokenValidator
+        // It needs to return a service name that would be in your "authorisedServices" list.
+        when(authTokenValidator.getServiceName("Bearer some-s2s-token"))
+            .thenReturn("some_authorised_service");
+
+        // 2. Mock the service that provides the data for the controller's response
+        StoredDocument doc1 = new StoredDocument();
+        doc1.setId(UUID.fromString("11111111-1111-1111-1111-111111111111"));
+        doc1.setClassification(Classifications.PUBLIC);
+        doc1.setCreatedBy("test-user-1");
+        // ... set other doc1 properties
+
+        StoredDocument doc2 = new StoredDocument();
+        doc2.setId(UUID.fromString("22222222-2222-2222-2222-222222222222"));
+        doc2.setClassification(Classifications.PUBLIC);
+        doc2.setCreatedBy("test-user-2");
+        // ... set other doc2 properties
+
+        when(auditedStoredDocumentOperationsService.createStoredDocuments(any()))
+            .thenReturn(List.of(doc1, doc2));
+    }
+}
