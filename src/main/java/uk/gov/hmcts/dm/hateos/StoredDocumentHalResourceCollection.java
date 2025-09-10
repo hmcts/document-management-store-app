8a87b96424f36becf5eed339cbfe739e67fb1e89
mdayican <mdayican@gmail.com>
2025-09-10 08:37:21 +0100

    fix style

--- src/main/java/uk/gov/hmcts/dm/hateos/StoredDocumentHalResourceCollection.java
+++ src/main/java/uk/gov/hmcts/dm/hateos/StoredDocumentHalResourceCollection.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.dm.hateos;
 
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.RepresentationModel;
 import uk.gov.hmcts.dm.domain.StoredDocument;
 
 import java.util.List;
