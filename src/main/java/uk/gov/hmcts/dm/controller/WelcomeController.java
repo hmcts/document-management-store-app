6cbde933e5af8ba24e54521648ca4394195b5e4b
mdayican <mdayican@gmail.com>
2025-11-13 07:15:14 +0000

    fix checkstyle

--- src/main/java/uk/gov/hmcts/dm/controller/WelcomeController.java
+++ src/main/java/uk/gov/hmcts/dm/controller/WelcomeController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
