bdba2186be672eb1a8d98f858dac1707b8f2d900
michaeljacob <Michael.Jacob@HMCTS.NET>
2026-01-22 15:15:16 +0000

    extend base

--- src/integrationTest/java/uk/gov/hmcts/dm/openapi/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/dm/openapi/OpenAPIPublisherTest.java
@@ -3,13 +3,9 @@ package uk.gov.hmcts.dm.openapi;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.dm.config.batch.CaseDocumentsDeletionTask;
+import uk.gov.hmcts.dm.componenttests.ComponentTestBase;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -18,21 +14,10 @@ import java.nio.file.Paths;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Built-in feature which saves service's swagger specs in temporary directory.
- * Each travis run on master should automatically save and upload (if updated) documentation.
- */
-@SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-@EnableAutoConfiguration(exclude = {
-    FlywayAutoConfiguration.class
-})
-class OpenAPIPublisherTest {
+class OpenAPIPublisherTest extends ComponentTestBase {
 
     private final MockMvc mockMvc;
-
-    @MockitoBean
-    private CaseDocumentsDeletionTask caseDocumentsDeletionTask;
 
     @Autowired
     OpenAPIPublisherTest(MockMvc mockMvc) {
