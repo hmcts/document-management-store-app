e781efef49b3ab4fba9e319eb9b29f3641517f6d
michaeljacob <Michael.Jacob@HMCTS.NET>
2026-01-22 13:57:47 +0000

    publish openapi

---
+++ src/integrationTest/java/uk/gov/hmcts/dm/openapi/OpenAPIPublisherTest.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.dm.openapi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Built-in feature which saves service's swagger specs in temporary directory.
+ * Each travis run on master should automatically save and upload (if updated) documentation.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@EnableAutoConfiguration(exclude = {
+    FlywayAutoConfiguration.class
+})
+class OpenAPIPublisherTest {
+
+    private final MockMvc mockMvc;
+
+    @MockitoBean
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @Autowired
+    OpenAPIPublisherTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("Generate swagger documentation")
+    @Test
+    void generateDocs() throws Exception {
+        byte[] specs = mockMvc.perform(get("/v3/api-docs"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/openapi-specs.json"))) {
+            outputStream.write(specs);
+        }
+    }
+}
