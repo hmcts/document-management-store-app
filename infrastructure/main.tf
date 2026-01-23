2ba0c52d840995b0a6a44fcb232ca0d6c2eef7cd
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2026-01-23 16:37:12 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -162,18 +162,18 @@ module "db-v15" {
   providers = {
     azurerm.postgres_network = azurerm.cft_vnet
   }
-  source                      = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
-  env                         = var.env
-  product                     = var.product
-  component                   = var.component
-  common_tags                 = var.common_tags
-  name                        = local.db_name
-  pgsql_version               = "15"
-  admin_user_object_id        = var.jenkins_AAD_objectId
-  business_area               = "CFT"
-  action_group_name           = join("-", [local.db_name, var.action_group_name, var.env])
-  email_address_key           = var.email_address_key
-  email_address_key_vault_id  = data.azurerm_key_vault.dm_shared_vault.id
+  source                     = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env                        = var.env
+  product                    = var.product
+  component                  = var.component
+  common_tags                = var.common_tags
+  name                       = local.db_name
+  pgsql_version              = "15"
+  admin_user_object_id       = var.jenkins_AAD_objectId
+  business_area              = "CFT"
+  action_group_name          = join("-", [local.db_name, var.action_group_name, var.env])
+  email_address_key          = var.email_address_key
+  email_address_key_vault_id = data.azurerm_key_vault.dm_shared_vault.id
   # The original subnet is full, this is required to use the new subnet for new databases
   subnet_suffix = "expanded"
   pgsql_databases = [
