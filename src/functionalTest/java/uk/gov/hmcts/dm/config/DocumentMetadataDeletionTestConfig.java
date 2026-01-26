f071893b9fae1bcb32a7d9e49449cc7bf21ba7c5
mdayican <mdayican@gmail.com>
2026-01-26 14:31:56 +0000

    use Configuration

--- src/functionalTest/java/uk/gov/hmcts/dm/config/DocumentMetadataDeletionTestConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/dm/config/DocumentMetadataDeletionTestConfig.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.dm.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -21,6 +22,7 @@ import uk.gov.hmcts.reform.idam.client.IdamClient;
     "uk.gov.hmcts.reform.idam.client"
 })
 @Import({
+    FeignAutoConfiguration.class,
     EmAnnoService.class,
     EmNpaService.class
 })
