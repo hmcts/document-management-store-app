7b9653aff2d6e11bd9ef77351c74b91497154997
mdayican <mdayican@gmail.com>
2026-01-26 10:08:36 +0000

    chnage test structure

--- src/functionalTest/java/uk/gov/hmcts/dm/functional/DocumentMetadataDeletionIT.java
+++ src/functionalTest/java/uk/gov/hmcts/dm/functional/DocumentMetadataDeletionIT.java
@@ -1,9 +1,18 @@
 package uk.gov.hmcts.dm.functional;
 
+import net.jcip.annotations.NotThreadSafe;
+import net.serenitybdd.annotations.WithTag;
+import net.serenitybdd.annotations.WithTags;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.dm.StorageTestConfiguration;
 import uk.gov.hmcts.dm.config.DocumentMetadataDeletionTestConfiguration;
+import uk.gov.hmcts.dm.config.ToggleConfiguration;
 import uk.gov.hmcts.dm.service.DocumentMetadataDeletionService;
 import uk.gov.hmcts.dm.service.EmAnnoService;
 import uk.gov.hmcts.dm.service.EmNpaService;
@@ -16,9 +25,20 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * Functional test for DocumentMetadataDeletionService.
  * This test verifies that the service can successfully call em-anno and em-npa endpoints
  * with proper authentication (S2S and IDAM tokens) handled internally by the service.
+ * This test uses its own isolated Spring context to avoid affecting other functional tests.
  */
-@Import({DocumentMetadataDeletionTestConfiguration.class, EmAnnoService.class, EmNpaService.class})
-public class DocumentMetadataDeletionIT extends BaseIT {
+@NotThreadSafe
+@ExtendWith(value = {SerenityJUnit5Extension.class, SpringExtension.class})
+@SpringBootTest(classes = {
+    DocumentMetadataDeletionTestConfiguration.class,
+    EmAnnoService.class,
+    EmNpaService.class,
+    StorageTestConfiguration.class,
+    ToggleConfiguration.class
+})
+@TestPropertySource({"classpath:application.yml"})
+@WithTags(@WithTag("testType:Functional"))
+public class DocumentMetadataDeletionIT {
 
     @Autowired
     private DocumentMetadataDeletionService documentMetadataDeletionService;
