5ebb2d0ea56970df16ba201527f95e2c3c945b9b
Asaleem-hmcts <Aamir.Saleem@HMCTS.NET>
2026-08-12 12:09:22 +0100

    adding import back

--- infrastructure/import.tf
+++ infrastructure/import.tf
@@ -1,6 +1,6 @@
 # This section is for Terraform import 
 
-#import {
-#  to = azurerm_storage_container.document_container
-#  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/ccd-shared-aat/providers/Microsoft.Storage/storageAccounts/dmstoredocaat/blobServices/default/containers/dm-store-docstore-aat"
-#}
+import {
+  to = azurerm_storage_container.document_container
+  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/ccd-shared-aat/providers/Microsoft.Storage/storageAccounts/dmstoredocaat/blobServices/default/containers/dm-store-docstore-aat"
+}
