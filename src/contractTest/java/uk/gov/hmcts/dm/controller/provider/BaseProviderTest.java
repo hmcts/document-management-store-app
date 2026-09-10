359bc548586c046a951c959205f96a3df04c3838
Daniel Lindsay <daniel.lindsay@HMCTS.NET>
2026-09-10 17:30:12 +0100

    CME-1083 Fixed integration test failures

--- src/contractTest/java/uk/gov/hmcts/dm/controller/provider/BaseProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/dm/controller/provider/BaseProviderTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
