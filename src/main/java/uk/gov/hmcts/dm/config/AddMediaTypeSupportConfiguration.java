35e34096cae09d8fa6f00ff1321485b294ff11bf
Daniel Lindsay <daniel.lindsay@HMCTS.NET>
2026-09-10 17:30:12 +0100

    CME-1083 Fixed integration test failures

--- src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.dm.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.hateoas.server.mvc.TypeConstrainedJacksonJsonHttpMessageConverter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.ArrayList;
@@ -18,33 +18,39 @@ public class AddMediaTypeSupportConfiguration implements BeanPostProcessor {
     public Object postProcessBeforeInitialization(Object bean, String s) {
         if (bean instanceof RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
             requestMappingHandlerAdapter.getMessageConverters().stream()
-                .filter(MappingJackson2HttpMessageConverter.class::isInstance)
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter
+                    || converter instanceof TypeConstrainedJacksonJsonHttpMessageConverter)
                 .forEach(converter -> {
-                    MappingJackson2HttpMessageConverter halConverterCandidate =
-                        (MappingJackson2HttpMessageConverter) converter;
-                    ObjectMapper objectMapper = halConverterCandidate.getObjectMapper();
-                    if (Jackson2HalModule.isAlreadyRegisteredIn(objectMapper)) {
-                        List<MediaType> vendorSpecificTypes =
-                            new ArrayList<>(halConverterCandidate.getSupportedMediaTypes());
-                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_COLLECTION_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_MEDIA_TYPE);
-
-                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_AND_METADATA_COLLECTION_MEDIA_TYPE);
-
-                        vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_COLLECTION_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_AUDIT_ENTRY_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_HAL_DOCUMENT_PAGE_MEDIA_TYPE);
-                        vendorSpecificTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_COLLECTION_MEDIA_TYPE);
-                        ((MappingJackson2HttpMessageConverter) converter).setSupportedMediaTypes(vendorSpecificTypes);
-                    }
+                    List<MediaType> vendorSpecificTypes =
+                        new ArrayList<>(converter.getSupportedMediaTypes());
+                    addVendorSpecificMediaTypes(vendorSpecificTypes);
+                    setSupportedMediaTypes(converter, vendorSpecificTypes);
                 });
         }
         return bean;
 
+    }
+
+    private void setSupportedMediaTypes(HttpMessageConverter<?> converter, List<MediaType> mediaTypes) {
+        if (converter instanceof MappingJackson2HttpMessageConverter jackson2Converter) {
+            jackson2Converter.setSupportedMediaTypes(mediaTypes);
+        } else if (converter instanceof TypeConstrainedJacksonJsonHttpMessageConverter hateoasConverter) {
+            hateoasConverter.setSupportedMediaTypes(mediaTypes);
+        }
+    }
+
+    private void addVendorSpecificMediaTypes(List<MediaType> mediaTypes) {
+        mediaTypes.add(V1MediaType.V1_HAL_DOCUMENT_COLLECTION_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_HAL_DOCUMENT_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_HAL_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_HAL_DOCUMENT_AND_METADATA_COLLECTION_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_DOCUMENT_COLLECTION_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_DOCUMENT_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_DOCUMENT_CONTENT_VERSION_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_AUDIT_ENTRY_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_HAL_DOCUMENT_PAGE_MEDIA_TYPE);
+        mediaTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_COLLECTION_MEDIA_TYPE);
     }
 
     @Override
