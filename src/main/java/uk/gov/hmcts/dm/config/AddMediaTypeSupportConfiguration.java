86456dbe78ca9a9bc02cb903502f9716a21cd55e
Daniel Lindsay <daniel.lindsay@HMCTS.NET>
2026-09-21 11:54:18 +0100

    Revert "CME-1083 Changed AddMediaTypeSupportConfiguration from extending BeanPostProcessor to extending WebMvcConfigurer"
    This reverts commit c8cb726685493774b8208f788865a645cd6905c3.

--- src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
@@ -1,29 +1,34 @@
 package uk.gov.hmcts.dm.config;
 
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.server.mvc.TypeConstrainedJacksonJsonHttpMessageConverter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class AddMediaTypeSupportConfiguration implements WebMvcConfigurer {
+public class AddMediaTypeSupportConfiguration implements BeanPostProcessor {
 
     @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.stream()
-            .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter
-                || converter instanceof TypeConstrainedJacksonJsonHttpMessageConverter)
-            .forEach(converter -> {
-                List<MediaType> vendorSpecificTypes =
-                    new ArrayList<>(converter.getSupportedMediaTypes());
-                addVendorSpecificMediaTypes(vendorSpecificTypes);
-                setSupportedMediaTypes(converter, vendorSpecificTypes);
-            });
+    public Object postProcessBeforeInitialization(Object bean, String s) {
+        if (bean instanceof RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
+            requestMappingHandlerAdapter.getMessageConverters().stream()
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter
+                    || converter instanceof TypeConstrainedJacksonJsonHttpMessageConverter)
+                .forEach(converter -> {
+                    List<MediaType> vendorSpecificTypes =
+                        new ArrayList<>(converter.getSupportedMediaTypes());
+                    addVendorSpecificMediaTypes(vendorSpecificTypes);
+                    setSupportedMediaTypes(converter, vendorSpecificTypes);
+                });
+        }
+        return bean;
+
     }
 
     private void setSupportedMediaTypes(HttpMessageConverter<?> converter, List<MediaType> mediaTypes) {
@@ -46,5 +51,10 @@ public class AddMediaTypeSupportConfiguration implements WebMvcConfigurer {
         mediaTypes.add(V1MediaType.V1_AUDIT_ENTRY_MEDIA_TYPE);
         mediaTypes.add(V1MediaType.V1_HAL_DOCUMENT_PAGE_MEDIA_TYPE);
         mediaTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_COLLECTION_MEDIA_TYPE);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) {
+        return o;
     }
 }
