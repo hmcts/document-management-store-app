ed73c3e1028d546544cad3f7facee3142b72cf96
Daniel Lindsay <daniel.lindsay@HMCTS.NET>
2026-09-22 10:09:42 +0100

    CME-1083 Changed AddMediaTypeSupportConfiguration so that HAL media types are associated with the HATEOAS mapper

--- src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
+++ src/main/java/uk/gov/hmcts/dm/config/AddMediaTypeSupportConfiguration.java
@@ -2,40 +2,58 @@ package uk.gov.hmcts.dm.config;
 
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.mvc.TypeConstrainedJacksonJsonHttpMessageConverter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class AddMediaTypeSupportConfiguration implements BeanPostProcessor {
 
     @Override
-    public Object postProcessBeforeInitialization(Object bean, String s) {
-        if (bean instanceof RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
-            requestMappingHandlerAdapter.getMessageConverters().stream()
-                .filter(converter -> converter instanceof JacksonJsonHttpMessageConverter
-                    || converter instanceof TypeConstrainedJacksonJsonHttpMessageConverter)
-                .forEach(converter -> {
-                    List<MediaType> vendorSpecificTypes =
-                        new ArrayList<>(converter.getSupportedMediaTypes());
-                    addVendorSpecificMediaTypes(vendorSpecificTypes);
-                    setSupportedMediaTypes(converter, vendorSpecificTypes);
-                });
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        if (bean instanceof RequestMappingHandlerAdapter handlerAdapter) {
+            configureConverters(handlerAdapter.getMessageConverters());
         }
         return bean;
+    }
 
+    private void configureConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+            .filter(converter -> converter instanceof JacksonJsonHttpMessageConverter
+                || converter instanceof TypeConstrainedJacksonJsonHttpMessageConverter)
+            .forEach(converter -> {
+                List<MediaType> vendorSpecificTypes =
+                    new ArrayList<>(converter.getSupportedMediaTypes());
+                addVendorSpecificMediaTypes(vendorSpecificTypes);
+                setSupportedMediaTypes(converter, vendorSpecificTypes);
+                registerVendorSpecificMappers(converter, vendorSpecificTypes);
+            });
     }
 
     private void setSupportedMediaTypes(HttpMessageConverter<?> converter, List<MediaType> mediaTypes) {
         if (converter instanceof JacksonJsonHttpMessageConverter jacksonConverter) {
             jacksonConverter.setSupportedMediaTypes(mediaTypes);
-        } else if (converter instanceof TypeConstrainedJacksonJsonHttpMessageConverter hateoasConverter) {
-            hateoasConverter.setSupportedMediaTypes(mediaTypes);
+        }
+    }
+
+    private void registerVendorSpecificMappers(HttpMessageConverter<?> converter, List<MediaType> mediaTypes) {
+        if (converter instanceof JacksonJsonHttpMessageConverter jacksonConverter) {
+            Map<MediaType, JsonMapper> registeredMappers =
+                jacksonConverter.getMappersForType(RepresentationModel.class);
+
+            if (!registeredMappers.isEmpty()) {
+                JsonMapper hypermediaMapper = registeredMappers.values().iterator().next();
+                jacksonConverter.registerMappersForType(RepresentationModel.class,
+                    mappers -> mediaTypes.forEach(mediaType -> mappers.putIfAbsent(mediaType, hypermediaMapper)));
+            }
         }
     }
 
@@ -53,8 +71,4 @@ public class AddMediaTypeSupportConfiguration implements BeanPostProcessor {
         mediaTypes.add(V1MediaType.V1_HAL_AUDIT_ENTRY_COLLECTION_MEDIA_TYPE);
     }
 
-    @Override
-    public Object postProcessAfterInitialization(Object o, String s) {
-        return o;
-    }
 }
